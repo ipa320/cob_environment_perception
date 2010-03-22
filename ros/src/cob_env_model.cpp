a3a9d611007913780c2ad0a1a7f3083befbad701
Georg <goa@ikzorn.(none)>
2010-03-22 15:00:12 +0100

    eclipse project files

--- ros/src/cob_env_model.cpp
+++ ros/src/cob_env_model.cpp
@@ -68,96 +68,146 @@
 #include <std_msgs/String.h>
 #include <cob3_msgs/ColoredPointCloud.h>
 
+#include <cob3_srvs/GetColoredPointCloud.h>
+#include <cob3_srvs/Trigger.h>
+
 
 // external includes
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
 #include "cob_sensor_fusion/ColoredPointCloud.h"
+#include "cob_env_model/CuiEnvReconstruction.h"
 
 
 //####################
 //#### node class ####
-class NodeClass
+class CobEnvModelNode
 {
-    //
+
     public:
-	    // create a handle for this node, initialize node
-	    ros::NodeHandle n;
-                
-        // topics to publish
-        ros::Publisher topicPub_demoPublish;
-        
-	    // topics to subscribe, callback is called for new messages arriving
-        ros::Subscriber topicSub_coloredPointCloud;
-				tf::TransformListener listener;
-
-	//Matrix m_transformCam2Base;
-				ipa_SensorFusion::ColoredPointCloud m_ColoredPointCloud;
-	IplImage m_GreyImage;
-	sensor_msgs::CvBridge m_Bridge;
-
-        // service servers
-        //--
-            
-        // service clients
-        //--
-        
-        // global variables
-        //--
 
         // Constructor
-        NodeClass()
-        {
-            topicPub_demoPublish = n.advertise<std_msgs::String>("demoPublish", 1);
-            topicSub_coloredPointCloud = n.subscribe("coloredPointCloud", 1, &NodeClass::topicCallback_coloredPointCloud, this);
-        }
+		CobEnvModelNode();
+
         
         // Destructor
-        ~NodeClass() 
+        ~CobEnvModelNode()
         {
+        	/// void
         }
+
+        unsigned long Init();
 
         // topic callback functions 
         // function will be called when a new message arrives on a topic
-        void topicCallback_coloredPointCloud(const cob3_msgs::ColoredPointCloud::ConstPtr& msg)
-        {
-					tf::StampedTransform transform;
-    			try{
-      			listener.lookupTransform("/cam", "/base", ros::Time(0), transform);
-    			}
-    			catch (tf::TransformException ex){
-      			ROS_ERROR("%s",ex.what());
-    			}
-				//transform in Matrix m_TransformCam2Base umwandeln	
+        void topicCallback_coloredPointCloud(const cob3_msgs::ColoredPointCloud::ConstPtr& msg);
 
-						//ColoredPointCloud Objekt füllen aus msg
-						EnvReconstruction::UpdateFilter;
-            ROS_INFO("this is topicCallback_demoSubscribe");
-        }
 
-	void UpdateColoredPointCloud(const cob3_msgs::ColoredPointCloud::ConstPtr& msg)
-	{
-		IplImage* colorImage = m_Bridge.imgMsgToCv((sensor_msgs::ImageConstPtr)(&(msg->colorImage)));
-		m_ColoredPointCloud.SetColorImage(colorImage);
-		IplImage* xyzImage = m_Bridge.imgMsgToCv((sensor_msgs::ImageConstPtr)(&(msg->pointCloud)));
-		m_ColoredPointCloud.SetXYZImage(xyzImage);
-	}
+        void UpdateColoredPointCloud(const cob3_msgs::ColoredPointCloud::ConstPtr& msg);
+
+        bool srvCallback_UpdateEnvModel(cob3_srvs::Trigger::Request &req,
+										cob3_srvs::Trigger::Response &res);
+
+
+		// create a handle for this node, initialize node
+		ros::NodeHandle n;
+
+		// topics to publish
+		ros::Publisher topicPub_demoPublish;
+
+		// topics to subscribe, callback is called for new messages arriving
+		ros::Subscriber topicSub_coloredPointCloud;
+		tf::TransformListener listener;
+
+		ros::ServiceServer srvServer_Trigger;
+		ros::ServiceClient srvClient_ColoredPointCloud;
+
+		//CuiEnvReconstruction m_EnvReconstruction;
+
+    protected:
+
+		//Matrix m_transformCam2Base;
+		ipa_SensorFusion::ColoredPointCloud m_ColoredPointCloud;
+		IplImage* m_GreyImage;
+		sensor_msgs::CvBridge m_Bridge;
+
+		cob3_srvs::GetColoredPointCloud m_ColoredPointCloudSrv;
 
 };
+
+
+CobEnvModelNode::CobEnvModelNode()
+{
+    //topicPub_demoPublish = n.advertise<std_msgs::String>("demoPublish", 1);
+    topicSub_coloredPointCloud = n.subscribe("coloredPointCloud", 1, &CobEnvModelNode::topicCallback_coloredPointCloud, this);
+    srvClient_ColoredPointCloud = n.serviceClient<cob3_srvs::GetColoredPointCloud>("GetColoredPointCloud");
+    srvServer_Trigger = n.advertiseService("UpdateEnvModel", &CobEnvModelNode::srvCallback_UpdateEnvModel, this);
+}
+
+unsigned long CobEnvModelNode::Init()
+{
+	/*if (m_EnvReconstruction.Init() & ipa_Utils::RET_FAILED)
+	{
+		std::cerr << "ERROR - Main:" << std::endl;
+		std::cerr << "\t ... Error while initializing EnvReconstruction.\n";
+		return -1;
+	}*/
+}
+
+void CobEnvModelNode::topicCallback_coloredPointCloud(const cob3_msgs::ColoredPointCloud::ConstPtr& msg)
+{
+	tf::StampedTransform transform;
+	try
+	{
+		listener.lookupTransform("/cam", "/base", ros::Time(0), transform);
+	}
+	catch (tf::TransformException ex)
+	{
+		ROS_ERROR("%s",ex.what());
+	}
+	//transform in Matrix m_TransformCam2Base umwandeln
+
+	//UpdateColoredPointCloud(msg);
+	//ColoredPointCloud Objekt füllen aus msg
+	//EnvReconstruction::UpdateFilter;
+    ROS_INFO("this is topicCallback_demoSubscribe");
+}
+
+void CobEnvModelNode::UpdateColoredPointCloud(const cob3_msgs::ColoredPointCloud::ConstPtr& msg)
+{
+	IplImage* colorImage = m_Bridge.imgMsgToCv((sensor_msgs::ImageConstPtr)(&(msg->colorImage)));
+	m_ColoredPointCloud.SetColorImage(colorImage);
+	IplImage* xyzImage = m_Bridge.imgMsgToCv((sensor_msgs::ImageConstPtr)(&(msg->pointCloud)));
+	m_ColoredPointCloud.SetXYZImage(xyzImage);
+	m_GreyImage = m_Bridge.imgMsgToCv((sensor_msgs::ImageConstPtr)(&(msg->greyImage)));
+}
+
+bool CobEnvModelNode::srvCallback_UpdateEnvModel(cob3_srvs::Trigger::Request &req,
+								cob3_srvs::Trigger::Response &res)
+{
+	srvClient_ColoredPointCloud.call(m_ColoredPointCloudSrv);
+	//warten?
+	//UpdateColoredPointCloud(&(m_ColoredPointCloudSrv.response.ColoredPointCloud));
+}
 
 //#######################
 //#### main programm ####
 int main(int argc, char** argv)
 {
     // initialize ROS, spezify name of node
-    ros::init(argc, argv, "node_name");
+    ros::init(argc, argv, "cobEnvModelNode");
     
-    NodeClass nodeClass;
+    CobEnvModelNode cobEnvModelNode;
  
-    while(nodeClass.n.ok())
+    while(cobEnvModelNode.n.ok())
     {
-
         ros::spinOnce();
+        /*if (cobEnvModelNode.m_EnvReconstruction.UI() & ipa_Utils::RET_FAILED)
+        {
+    		std::cerr << "ERROR - Main:" << std::endl;
+    		std::cerr << "\t ... Error in cobEnvModelNode.m_EnvReconstruction.UI().\n";
+        	break;
+        }*/
     }
     
 //    ros::spin();
