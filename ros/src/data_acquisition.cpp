833dd0d05ae5b9546d48e288f7ebd1633b1d714e
Georg <goa@ikzorn.(none)>
2010-04-15 16:40:04 +0200

    data acquisition node now works

--- ros/src/data_acquisition.cpp
+++ ros/src/data_acquisition.cpp
@@ -98,157 +98,24 @@ class CobDataAcquisitionNode
 
     public:
 
-        // Constructor
-		CobDataAcquisitionNode()
-			: image_transport_(n),
-			  shared_sub_sync_(SharedSyncPolicy(3)),
-			  right_color_image_8U3_(0),
-			  xyz_image_32F3_(0),
-			  grey_image_32F1_(0),
-			  robot_pose_(3),
-				transformation_camera2base_(4,4)
-		{
-			init();
-		    //topicPub_demoPublish = n.advertise<std_msgs::String>("demoPublish", 1);
-		    //topicSub_coloredPointCloud = n.subscribe("/sensor_fusion/ColoredPointCloud", 1, &CobEnvModelNode::topicCallback_coloredPointCloud, this);
-		    srv_client_platform_position_ = n.serviceClient<cob_srvs::GetPlatformPosition>("/get_platform_position");
-		    srv_client_transform_camera2base_ = n.serviceClient<cob_srvs::GetTransformCamera2Base>("/transform_camera2base");
-		    //srv_server_trigger_ = n.advertiseService("UpdateEnvModel", &CobDataAcquisitionNode::srvCallback_UpdateEnvModel, this);
+		CobDataAcquisitionNode(ros::NodeHandle n);
 
-		}
+		~CobDataAcquisitionNode();
 
+		unsigned long init();
 
-        // Destructor
-        ~CobDataAcquisitionNode()
-        {
-    		if (right_color_image_8U3_) cvReleaseImage(&right_color_image_8U3_);
-    		if (xyz_image_32F3_) cvReleaseImage(&xyz_image_32F3_);
-    		if (grey_image_32F1_) cvReleaseImage(&grey_image_32F1_);
-        }
+		unsigned long getRobotPose();
 
-        unsigned long init()
-        {
-			shared_sub_sync_.connectInput(right_color_camera_image_sub_, tof_camera_xyz_image_sub_, tof_camera_grey_image_sub_);
-			shared_sub_sync_.registerCallback(boost::bind(&CobDataAcquisitionNode::sharedModeSrvCallback, this, _1, _2, _3));
-			right_color_camera_image_sub_.subscribe(image_transport_, "right/image_color", 1);
-			tof_camera_xyz_image_sub_.subscribe(image_transport_, "image_xyz", 1);
-			tof_camera_grey_image_sub_.subscribe(image_transport_, "image_grey", 1);
-        	return ipa_Utils::RET_OK;
-        }
+		unsigned long getTransformationCam2Base();
 
-    	unsigned long getRobotPose()
-    	{
-    		if(srv_client_platform_position_.call(platform_position_srv_))
-    		{
-    			ROS_INFO("[env_model_node] Platform position service called [OK].");
-    		}
-    		else
-    		{
-    			ROS_ERROR("[env_model_node] Platform position service called [FAILED].");
-    		}
+		void sharedModeSrvCallback(const sensor_msgs::ImageConstPtr& right_camera_data,
+		    			const sensor_msgs::ImageConstPtr& tof_camera_xyz_data,
+		    			const sensor_msgs::ImageConstPtr& tof_camera_grey_data);
 
-    		ROS_INFO("[env_model_node] Platform position (x,y,theta): %f, %f, %f",
-    				platform_position_srv_.response.platform_pose.x, platform_position_srv_.response.platform_pose.y,
-    				platform_position_srv_.response.platform_pose.theta);
+		bool srvCallback_AcquireData(cob_srvs::Trigger::Request &req,
+						cob_srvs::Trigger::Response &res);
 
-    		robot_pose_(1) = platform_position_srv_.response.platform_pose.x;
-    		robot_pose_(2) = platform_position_srv_.response.platform_pose.y;
-    		robot_pose_(3) = platform_position_srv_.response.platform_pose.theta;
-    		return ipa_Utils::RET_OK;
-    	}
-
-    	unsigned long getTransformationCam2Base()
-    	{
-    		if(srv_client_transform_camera2base_.call(transform_camera2base_srv_))
-    		{
-    			ROS_INFO("[env_model_node] Transformation Camera2Base service called [OK].");
-    		}
-    		else
-    		{
-    			ROS_ERROR("[env_model_node] Transformation Camera2Base service called [FAILED].");
-    		}
-    		ROS_INFO("[env_model_node] Transformation Camera2Base (x,y,z,roll,pitch,yaw): %f, %f, %f, %f, %f, %f",
-    					transform_camera2base_srv_.response.transformation.x, transform_camera2base_srv_.response.transformation.y,
-    					transform_camera2base_srv_.response.transformation.z, transform_camera2base_srv_.response.transformation.roll,
-    					transform_camera2base_srv_.response.transformation.pitch, transform_camera2base_srv_.response.transformation.yaw);
-    		double yaw = transform_camera2base_srv_.response.transformation.yaw;
-    		double pitch = transform_camera2base_srv_.response.transformation.pitch;
-    		double roll = transform_camera2base_srv_.response.transformation.roll;
-
-    		transformation_camera2base_(1,1) = cos(yaw)*cos(pitch);
-    		transformation_camera2base_(1,2) = cos(yaw)*sin(pitch)*sin(roll)-sin(yaw)*sin(roll);
-    		transformation_camera2base_(1,3) = cos(yaw)*sin(pitch)*cos(roll)+sin(yaw)*sin(roll);
-    		transformation_camera2base_(1,4) = transform_camera2base_srv_.response.transformation.x;
-    		transformation_camera2base_(2,1) = sin(yaw)*cos(pitch);
-    		transformation_camera2base_(2,2) = sin(yaw)*sin(pitch)*sin(roll)+cos(yaw)*sin(roll);
-    		transformation_camera2base_(2,3) = sin(yaw)*sin(pitch)*cos(roll)-cos(yaw)*sin(roll);
-    		transformation_camera2base_(2,4) = transform_camera2base_srv_.response.transformation.y;
-    		transformation_camera2base_(3,1) = -sin(pitch);
-    		transformation_camera2base_(3,2) = cos(pitch)*sin(roll);
-    		transformation_camera2base_(3,3) = cos(pitch)*cos(roll);
-    		transformation_camera2base_(3,4) = transform_camera2base_srv_.response.transformation.z;
-    		transformation_camera2base_(4,1) = 0;
-    		transformation_camera2base_(4,2) = 0;
-    		transformation_camera2base_(4,3) = 0;
-    		transformation_camera2base_(4,4) = 1;
-    		return ipa_Utils::RET_OK;
-    	}
-
-    	void sharedModeSrvCallback(const sensor_msgs::ImageConstPtr& right_camera_data,
-    			const sensor_msgs::ImageConstPtr& tof_camera_xyz_data,
-    			const sensor_msgs::ImageConstPtr& tof_camera_grey_data)
-    	{
-    		ROS_INFO("[data_acquisition] receiving image data");
-    		// Convert ROS image messages to openCV IplImages
-    		if(right_color_image_8U3_) cvReleaseImage(&right_color_image_8U3_);
-    		if(xyz_image_32F3_) cvReleaseImage(&xyz_image_32F3_);
-    		if(grey_image_32F1_) cvReleaseImage(&grey_image_32F1_);
-    		try
-    		{
-    			right_color_image_8U3_ = cvCloneImage(cv_bridge_0_.imgMsgToCv(right_camera_data, "passthrough"));
-    			xyz_image_32F3_ = cvCloneImage(cv_bridge_1_.imgMsgToCv(tof_camera_xyz_data, "passthrough"));
-    			grey_image_32F1_ = cvCloneImage(cv_bridge_2_.imgMsgToCv(tof_camera_grey_data, "passthrough"));
-    		}
-			catch (sensor_msgs::CvBridgeException& e)
-			{
-			  ROS_ERROR("[data_acquisition] Could not convert images by cv_bridge.");
-			}
-    		ROS_INFO("OK");
-
-    	}
-
-    	unsigned long save()
-    	{
-    		char counterBuffer [50];
-    		std::string imageFileName;
-    		std::string filetype = "bmp";
-    		static unsigned int counter=1;
-    		sprintf(counterBuffer, "%04d", counter);
-    	    if (right_color_image_8U3_)
-    	    {
-				imageFileName = std::string("ColorCamRGB_8U3_0_") + counterBuffer + "." + filetype;
-				std::cout << "\t ... Image of right color camera saved to " + imageFileName << std::endl;
-				cvSaveImage(imageFileName.c_str() , right_color_image_8U3_);
-    	    }
-    	    if (xyz_image_32F3_)
-    	    {
-				imageFileName = std::string("RangeCamCoordinate_32F3_0_") + counterBuffer + ".xml";
-				std::cout << "\t ... TOF coordinate Image saved to " + imageFileName << std::endl;
-				cvSave(imageFileName.c_str() , xyz_image_32F3_);
-    	    }
-    	    if (grey_image_32F1_)
-    	    {
-				imageFileName = std::string("RangeCamIntensity_32F1_0_") + counterBuffer + ".xml";
-				std::cout << "\t ... TOF grey Image saved to " + imageFileName << std::endl;
-				cvSave(imageFileName.c_str() , grey_image_32F1_);
-    	    }
-    	    std::string pathFileName = std::string("path_and_trafo") + counterBuffer + ".txt";
-    	    std::ofstream f(pathFileName.c_str(), std::fstream::out);
-    	    f << robot_pose_ << "\n";
-    	    f << transformation_camera2base_;
-    	    f.close();
-    	    counter++;
-    	}
+		unsigned long save();
 
 		// create a handle for this node, initialize node
 		ros::NodeHandle n;
@@ -279,37 +146,223 @@ class CobDataAcquisitionNode
 		sensor_msgs::CvBridge cv_bridge_1_; ///< Converts ROS image messages to openCV IplImages
 		sensor_msgs::CvBridge cv_bridge_2_; ///< Converts ROS image messages to openCV IplImages
 
-
 		MatrixWrapper::ColumnVector robot_pose_;
 		MatrixWrapper::Matrix transformation_camera2base_;
+
+		std::string data_directory_;
 };
+
+// Constructor
+CobDataAcquisitionNode::CobDataAcquisitionNode(ros::NodeHandle n)
+	: image_transport_(n),
+	  shared_sub_sync_(SharedSyncPolicy(3)),
+	  right_color_image_8U3_(0),
+	  xyz_image_32F3_(0),
+	  grey_image_32F1_(0),
+	  robot_pose_(3),
+	  transformation_camera2base_(4,4)
+{
+	init();
+	transformation_camera2base_=0;
+	robot_pose_=0;
+	//topicPub_demoPublish = n.advertise<std_msgs::String>("demoPublish", 1);
+	//topicSub_coloredPointCloud = n.subscribe("/sensor_fusion/ColoredPointCloud", 1, &CobEnvModelNode::topicCallback_coloredPointCloud, this);
+	srv_client_platform_position_ = n.serviceClient<cob_srvs::GetPlatformPosition>("/get_platform_position");
+	srv_client_transform_camera2base_ = n.serviceClient<cob_srvs::GetTransformCamera2Base>("/transform_camera2base");
+	srv_server_trigger_ = n.advertiseService("AcquireData", &CobDataAcquisitionNode::srvCallback_AcquireData, this);
+
+}
+
+
+// Destructor
+CobDataAcquisitionNode::~CobDataAcquisitionNode()
+{
+	if (right_color_image_8U3_) cvReleaseImage(&right_color_image_8U3_);
+	if (xyz_image_32F3_) cvReleaseImage(&xyz_image_32F3_);
+	if (grey_image_32F1_) cvReleaseImage(&grey_image_32F1_);
+}
+
+unsigned long CobDataAcquisitionNode::init()
+{
+	shared_sub_sync_.connectInput(right_color_camera_image_sub_, tof_camera_xyz_image_sub_, tof_camera_grey_image_sub_);
+	shared_sub_sync_.registerCallback(boost::bind(&CobDataAcquisitionNode::sharedModeSrvCallback, this, _1, _2, _3));
+	right_color_camera_image_sub_.subscribe(image_transport_, "right/image_color", 1);
+	tof_camera_xyz_image_sub_.subscribe(image_transport_, "image_xyz", 1);
+	tof_camera_grey_image_sub_.subscribe(image_transport_, "image_grey", 1);
+
+    if (n.hasParam("data_acquisition/data_directory"))
+    {
+        n.getParam("data_acquisition/data_directory", data_directory_);
+    }
+    else
+    {
+        ROS_ERROR("Parameter data_directory not set");
+        data_directory_ = std::string("../../common/files/");
+    }
+	return ipa_Utils::RET_OK;
+}
+
+unsigned long CobDataAcquisitionNode::getRobotPose()
+{
+	if(srv_client_platform_position_.call(platform_position_srv_))
+	{
+		ROS_INFO("[env_model_node] Platform position service called [OK].");
+	}
+	else
+	{
+		ROS_ERROR("[env_model_node] Platform position service called [FAILED].");
+	}
+
+	ROS_INFO("[env_model_node] Platform position (x,y,theta): %f, %f, %f",
+			platform_position_srv_.response.platform_pose.x, platform_position_srv_.response.platform_pose.y,
+			platform_position_srv_.response.platform_pose.theta);
+
+	robot_pose_(1) = platform_position_srv_.response.platform_pose.x;
+	robot_pose_(2) = platform_position_srv_.response.platform_pose.y;
+	robot_pose_(3) = platform_position_srv_.response.platform_pose.theta;
+	return ipa_Utils::RET_OK;
+}
+
+unsigned long CobDataAcquisitionNode::getTransformationCam2Base()
+{
+	if(srv_client_transform_camera2base_.call(transform_camera2base_srv_))
+	{
+		ROS_INFO("[env_model_node] Transformation Camera2Base service called [OK].");
+	}
+	else
+	{
+		ROS_ERROR("[env_model_node] Transformation Camera2Base service called [FAILED].");
+	}
+	ROS_INFO("[env_model_node] Transformation Camera2Base (x,y,z,roll,pitch,yaw): %f, %f, %f, %f, %f, %f",
+				transform_camera2base_srv_.response.transformation.x, transform_camera2base_srv_.response.transformation.y,
+				transform_camera2base_srv_.response.transformation.z, transform_camera2base_srv_.response.transformation.roll,
+				transform_camera2base_srv_.response.transformation.pitch, transform_camera2base_srv_.response.transformation.yaw);
+	double yaw = transform_camera2base_srv_.response.transformation.yaw;
+	double pitch = transform_camera2base_srv_.response.transformation.pitch;
+	double roll = transform_camera2base_srv_.response.transformation.roll;
+
+	transformation_camera2base_(1,1) = cos(yaw)*cos(pitch);
+	transformation_camera2base_(1,2) = cos(yaw)*sin(pitch)*sin(roll)-sin(yaw)*sin(roll);
+	transformation_camera2base_(1,3) = cos(yaw)*sin(pitch)*cos(roll)+sin(yaw)*sin(roll);
+	transformation_camera2base_(1,4) = transform_camera2base_srv_.response.transformation.x;
+	transformation_camera2base_(2,1) = sin(yaw)*cos(pitch);
+	transformation_camera2base_(2,2) = sin(yaw)*sin(pitch)*sin(roll)+cos(yaw)*sin(roll);
+	transformation_camera2base_(2,3) = sin(yaw)*sin(pitch)*cos(roll)-cos(yaw)*sin(roll);
+	transformation_camera2base_(2,4) = transform_camera2base_srv_.response.transformation.y;
+	transformation_camera2base_(3,1) = -sin(pitch);
+	transformation_camera2base_(3,2) = cos(pitch)*sin(roll);
+	transformation_camera2base_(3,3) = cos(pitch)*cos(roll);
+	transformation_camera2base_(3,4) = transform_camera2base_srv_.response.transformation.z;
+	transformation_camera2base_(4,1) = 0;
+	transformation_camera2base_(4,2) = 0;
+	transformation_camera2base_(4,3) = 0;
+	transformation_camera2base_(4,4) = 1;
+	return ipa_Utils::RET_OK;
+}
+
+void CobDataAcquisitionNode::sharedModeSrvCallback(const sensor_msgs::ImageConstPtr& right_camera_data,
+		const sensor_msgs::ImageConstPtr& tof_camera_xyz_data,
+		const sensor_msgs::ImageConstPtr& tof_camera_grey_data)
+{
+	ROS_INFO("[data_acquisition] receiving image data");
+	// Convert ROS image messages to openCV IplImages
+	if(right_color_image_8U3_) cvReleaseImage(&right_color_image_8U3_);
+	if(xyz_image_32F3_) cvReleaseImage(&xyz_image_32F3_);
+	if(grey_image_32F1_) cvReleaseImage(&grey_image_32F1_);
+	try
+	{
+		right_color_image_8U3_ = cvCloneImage(cv_bridge_0_.imgMsgToCv(right_camera_data, "passthrough"));
+		xyz_image_32F3_ = cvCloneImage(cv_bridge_1_.imgMsgToCv(tof_camera_xyz_data, "passthrough"));
+		grey_image_32F1_ = cvCloneImage(cv_bridge_2_.imgMsgToCv(tof_camera_grey_data, "passthrough"));
+	}
+	catch (sensor_msgs::CvBridgeException& e)
+	{
+	  ROS_ERROR("[data_acquisition] Could not convert images by cv_bridge.");
+	}
+	ROS_INFO("OK");
+
+}
+
+bool CobDataAcquisitionNode::srvCallback_AcquireData(cob_srvs::Trigger::Request &req,
+		cob_srvs::Trigger::Response &res)
+{
+	ROS_INFO("srv command: %d",req.command);
+	// get transformation and robot pose
+	if(req.command == 0)
+	{
+		if(getRobotPose() == ipa_Utils::RET_FAILED)
+		{
+			ROS_ERROR("Could not get robot pose.");
+			return false;
+		}
+		if(getTransformationCam2Base() == ipa_Utils::RET_FAILED)
+		{
+			ROS_ERROR("Could not get transformation camera2base.");
+			return false;
+		}
+	}
+	// save the data to file
+	else if(req.command == 1)
+	{
+		if(right_color_image_8U3_ && xyz_image_32F3_ && grey_image_32F1_)
+		{
+			save();
+		}
+		else
+		{
+			ROS_ERROR("No image data available.");
+			return false;
+		}
+	}
+	return true;
+}
+
+unsigned long CobDataAcquisitionNode::save()
+{
+	char counterBuffer [50];
+	std::string imageFileName;
+	std::string filetype = "bmp";
+	static unsigned int counter=1;
+	sprintf(counterBuffer, "%04d", counter);
+	if (right_color_image_8U3_)
+	{
+		imageFileName = data_directory_ + std::string("/ColorCamRGB_8U3_0_") + counterBuffer + "." + filetype;
+		std::cout << "\t ... Image of right color camera saved to " + imageFileName << std::endl;
+		cvSaveImage(imageFileName.c_str() , right_color_image_8U3_);
+	}
+	if (xyz_image_32F3_)
+	{
+		imageFileName = data_directory_ + std::string("/RangeCamCoordinate_32F3_0_") + counterBuffer + ".xml";
+		std::cout << "\t ... TOF coordinate Image saved to " + imageFileName << std::endl;
+		cvSave(imageFileName.c_str() , xyz_image_32F3_);
+	}
+	if (grey_image_32F1_)
+	{
+		imageFileName = data_directory_ + std::string("/RangeCamIntensity_32F1_0_") + counterBuffer + ".xml";
+		std::cout << "\t ... TOF grey Image saved to " + imageFileName << std::endl;
+		cvSave(imageFileName.c_str() , grey_image_32F1_);
+	}
+	std::string poseFileName = data_directory_ + std::string("/pose_and_trafo") + counterBuffer + ".txt";
+	std::ofstream f(poseFileName.c_str(), std::fstream::out);
+	f << robot_pose_ << "\n";
+	f << transformation_camera2base_;
+	f.close();
+	std::cout << "\t ... Robot pose and transformation saved to " + poseFileName << std::endl;
+	counter++;
+}
+
 
 int main(int argc, char** argv)
 {
     // initialize ROS, specify name of node
     ros::init(argc, argv, "cobDataAcquisitionNode");
 
-    CobDataAcquisitionNode cobDataAcquisitionNode;
+    // Create a handle for this node, initialize node
+    ros::NodeHandle nh;
 
-	bool first = true;
-	int i=0;
-	char c='a';
+    CobDataAcquisitionNode cobDataAcquisitionNode(nh);
 
-    ros::Rate r(1);
-    while(cobDataAcquisitionNode.n.ok())
-    {
-        ros::spinOnce();
-        cobDataAcquisitionNode.getTransformationCam2Base();
-        std::cin >> c;
-        if(c=='s')
-        {
-        	cobDataAcquisitionNode.save();
-        	c = 'a';
-        }
-        //cobDataAcquisitionNode.getRobotPose();
-		//i++;
-    	r.sleep();
-    }
+    ros::spin();
 
     return 0;
 }
