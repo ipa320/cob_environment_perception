0709418fc8477329321c5ac24b4fcd490129b2ba
ipa-uhr-fm <felix_messmer@web.de>
2011-02-10 15:26:49 +0100

    commit after merge with review-320

---
+++ ros/src/aggregate_point_map.cpp
@@ -0,0 +1,350 @@
+/****************************************************************
+ *
+ * Copyright (c) 2010
+ *
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: care-o-bot
+ * ROS stack name: cob_vision
+ * ROS package name: cob_env_model
+ * Description:
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Author: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+ * Supervised by: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+ *
+ * Date of creation: 01/2011
+ * ToDo:
+ * Only update if new robot pose available
+ * Resample point cloud
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+
+//##################
+//#### includes ####
+
+// standard includes
+//--
+#include <sstream>
+
+// ROS includes
+#include <ros/ros.h>
+#include <pcl/io/io.h>
+#include <pcl/point_types.h>
+#include <pcl/registration/icp.h>
+#include <tf/transform_listener.h>
+#include <tf_conversions/tf_kdl.h>
+#include <cob_vision_ipa_utils/cpc_point.h>
+#include "pcl/filters/voxel_grid.h"
+#include <pcl_ros/transforms.h>
+#include <pcl_ros/point_cloud.h>
+
+// ROS message includes
+//#include <sensor_msgs/PointCloud2.h>
+
+// external includes
+#include <boost/timer.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+
+
+using namespace tf;
+
+//####################
+//#### node class ####
+class AggregatePointMap
+{
+public:
+    // Constructor
+	AggregatePointMap(const ros::NodeHandle& nh)
+	  :	n_(nh),
+	   	first_(true)
+	{
+		point_cloud_sub_ = n_.subscribe("point_cloud2", 1, &AggregatePointMap::pointCloudSubCallback, this);
+		point_cloud_pub_ = n_.advertise<pcl::PointCloud<pcl::PointXYZ> >("point_cloud2_map",1);
+	}
+
+
+    // Destructor
+    ~AggregatePointMap()
+    {
+    	/// void
+    }
+
+    void pointCloudSubCallback(const pcl::PointCloud<pcl::PointXYZ>::Ptr& pc)
+    {
+    	//ROS_INFO("PointCloudSubCallback");
+    	static int frame_ctr=0;
+
+    	StampedTransform transform;
+    	try{
+    		tf_listener_.lookupTransform("/map", pc->header.frame_id, ros::Time(0), transform);
+    		KDL::Frame frame_KDL, frame_KDL_old;
+    		tf::TransformTFToKDL(transform, frame_KDL);
+    		tf::TransformTFToKDL(transform_old_, frame_KDL_old);
+    		double r,p,y;
+    		frame_KDL.M.GetRPY(r,p,y);
+    		double r_old,p_old,y_old;
+    		frame_KDL_old.M.GetRPY(r_old,p_old,y_old);
+    		if(fabs(r-r_old) > 0.05 || fabs(p-p_old) > 0.05 || fabs(y-y_old) > 0.05 ||
+    				transform.getOrigin().distance(transform_old_.getOrigin()) > 0.1)
+    		{
+    			std::cout << "Registering new point cloud" << std::endl;
+    			transform_old_ = transform;
+				//transformPointCloud("/map", transform, pc->header.stamp, *(pc.get()), *(pc.get()));
+    			pcl_ros::transformPointCloud(*(pc.get()), *(pc.get()), transform);
+    			pc->header.frame_id = "/map";
+				if(first_)
+				{
+					map_ = *(pc.get());
+					map_.header.frame_id="/map";
+					first_ = false;
+				}
+				else
+					map_ += *(pc.get());
+				//pcl::PointCloud<pcl::PointXYZ>::Ptr msg(&map_);
+				//point_cloud_pub_.publish(msg);
+
+				//pcl::PointCloud<pcl::PointXYZ> pc_in;
+				//pcl::fromROSMsg(*(pc.get()), pc_in);
+				std::stringstream ss;
+				ss << "frame_" << frame_ctr << ".pcd";
+				pcl::io::savePCDFileASCII (ss.str(), *(pc.get()));
+				frame_ctr++;
+				//ROS_INFO("New map has %d points", map_.height*map_.width);
+				//pcl::io::savePCDFileASCII ("map.pcd", map_);
+    		}
+    		else
+    			ROS_INFO("Skipped");
+    	}
+    	catch (tf::TransformException ex){
+    		ROS_ERROR("%s",ex.what());
+    	}
+    }
+
+    void pointCloudSubCallbackICP(const pcl::PointCloud<pcl::PointXYZ>::Ptr& pc)
+    {
+    	//ROS_INFO("PointCloudSubCallback");
+
+    	StampedTransform transform;
+    	try{
+    		tf_listener_.lookupTransform("/map", pc->header.frame_id, ros::Time(0), transform);
+    		KDL::Frame frame_KDL, frame_KDL_old;
+    		tf::TransformTFToKDL(transform, frame_KDL);
+    		tf::TransformTFToKDL(transform_old_, frame_KDL_old);
+    		double r,p,y;
+    		frame_KDL.M.GetRPY(r,p,y);
+    		double r_old,p_old,y_old;
+    		frame_KDL_old.M.GetRPY(r_old,p_old,y_old);
+    		if(fabs(r-r_old) > 0.05 || fabs(p-p_old) > 0.05 || fabs(y-y_old) > 0.05 ||
+    				transform.getOrigin().distance(transform_old_.getOrigin()) > 0.1)
+    		{
+    			std::cout << "Registering new point cloud using ICP" << std::endl;
+    			transform_old_ = transform;
+    			pcl_ros::transformPointCloud(*(pc.get()), *(pc.get()), transform);
+				if(first_)
+				{
+					map_ = *(pc.get());
+					map_.header.frame_id="/map";
+					first_ = false;
+				}
+				else
+				{
+					//pcl::PointCloud<pcl::PointXYZ> pc_in, pc_map;
+					//pcl::PCDReader reader;
+					//reader.read ("/home/goa/git/cob3_intern/cob_sandbox/pcl_test/common/files/cob3-2/pcd_kitchen/kitchen_01_world.pcd", pc_in);
+					//reader.read ("/home/goa/git/cob3_intern/cob_sandbox/pcl_test/common/files/cob3-2/pcd_kitchen/kitchen_02_world.pcd", pc_map);
+					//pcl::fromROSMsg(*(pc.get()), pc_in);
+					//pcl::fromROSMsg(map_, pc_map);
+					pcl::IterativeClosestPoint<pcl::PointXYZ,pcl::PointXYZ> icp;
+					//pcl::VoxelGrid<CPCPoint> vox_filter;
+					icp.setInputCloud(pc);
+					icp.setInputTarget(map_.makeShared());
+					icp.setMaximumIterations(50);
+					icp.setMaxCorrespondenceDistance(0.1);
+					icp.setTransformationEpsilon (1e-6);
+					pcl::PointCloud<pcl::PointXYZ> pc_map_new;
+					icp.align(pc_map_new);
+					map_ += pc_map_new;
+					ROS_INFO("Aligned PC has %d points", map_.size());
+					//sensor_msgs::PointCloud2 pc_msg_map_new;
+					//pcl::toROSMsg(pc_map, map_);
+					//ROS_INFO("Aligned PC has %d points", pc_msg_map_new.height*pc_msg_map_new.width);
+					//ROS_INFO("Old map has %d points", map_.height*map_.width);
+					//pcl::concatenatePointCloud (map_, pc_msg_map_new, map_);
+					//pcl::fromROSMsg(map_, pc_map);
+					//ROS_INFO("New map has %d points", map_.height*map_.width);
+					pcl::io::savePCDFileASCII ("cloud_in.pcd", *(pc.get()));
+					pcl::io::savePCDFileASCII ("cloud_out.pcd", pc_map_new);
+					pcl::io::savePCDFileASCII ("map.pcd", map_);
+				}
+
+				//point_cloud_pub_.publish(map_);
+    		}
+    		else
+    			ROS_INFO("Skipped");
+    	}
+    	catch (tf::TransformException ex){
+    		ROS_ERROR("%s",ex.what());
+    	}
+    }
+
+    boost::numeric::ublas::matrix<double> transformAsMatrix(const Transform& bt)
+    {
+      boost::numeric::ublas::matrix<double> outMat(4,4);
+
+      //  double * mat = outMat.Store();
+
+      double mv[12];
+      bt.getBasis().getOpenGLSubMatrix(mv);
+
+      Vector3 origin = bt.getOrigin();
+
+      outMat(0,0)= mv[0];
+      outMat(0,1)  = mv[4];
+      outMat(0,2)  = mv[8];
+      outMat(1,0)  = mv[1];
+      outMat(1,1)  = mv[5];
+      outMat(1,2)  = mv[9];
+      outMat(2,0)  = mv[2];
+      outMat(2,1)  = mv[6];
+      outMat(2,2) = mv[10];
+
+      outMat(3,0)  = outMat(3,1) = outMat(3,2) = 0;
+      outMat(0,3) = origin.x();
+      outMat(1,3) = origin.y();
+      outMat(2,3) = origin.z();
+      outMat(3,3) = 1;
+
+
+      return outMat;
+    };
+
+
+    void transformPointCloud(const std::string & target_frame, const Transform& net_transform,
+    						const ros::Time& target_time, const sensor_msgs::PointCloud2 & cloudIn, sensor_msgs::PointCloud2 & cloudOut)
+    {
+    	boost::numeric::ublas::matrix<double> transform = transformAsMatrix(net_transform);
+
+		int x_offset = 0, y_offset = 0, z_offset = 0;
+		for (size_t d = 0; d < cloudIn.fields.size(); ++d)
+		{
+			if(cloudIn.fields[d].name == "x")
+				x_offset = cloudIn.fields[d].offset;
+			if(cloudIn.fields[d].name == "y")
+				y_offset = cloudIn.fields[d].offset;
+			if(cloudIn.fields[d].name == "z")
+				z_offset = cloudIn.fields[d].offset;
+		}
+
+      unsigned int length = cloudIn.height*cloudIn.width;
+
+      boost::numeric::ublas::matrix<float> matIn(4, length);
+
+      float * matrixPtr = matIn.data().begin();
+
+      for (unsigned int i = 0; i < length ; i++)
+      {
+    	  memcpy(&matrixPtr[i], &cloudIn.data[i * cloudIn.point_step + x_offset], sizeof(float));
+    	  memcpy(&matrixPtr[i+length], &cloudIn.data[i * cloudIn.point_step + y_offset], sizeof(float));
+    	  memcpy(&matrixPtr[i+ 2* length], &cloudIn.data[i * cloudIn.point_step + z_offset], sizeof(float));
+    	  matrixPtr[i+ 3* length] = 1;
+      }
+
+      boost::numeric::ublas::matrix<float> matOut = prod(transform, matIn);
+
+      // Copy relevant data from cloudIn, if needed
+      if (&cloudIn != &cloudOut)
+      {
+			cloudOut.header =  cloudIn.header;
+			cloudOut.fields =  cloudIn.fields;
+			cloudOut.width =  cloudIn.width;
+			cloudOut.height =  cloudIn.height;
+			cloudOut.is_bigendian =  cloudIn.is_bigendian;
+			cloudOut.point_step =  cloudIn.point_step;
+			cloudOut.row_step =  cloudOut.point_step * cloudOut.width;
+			cloudOut.is_dense =  cloudIn.is_dense;
+			cloudOut.data = cloudIn.data;
+      }
+
+      matrixPtr = matOut.data().begin();
+
+      //Override the positions
+      cloudOut.header.stamp = target_time;
+      cloudOut.header.frame_id = target_frame;
+      for (unsigned int i = 0; i < length ; i++)
+      {
+    	  memcpy(&cloudOut.data[i * cloudOut.point_step + x_offset], &matrixPtr[i], sizeof(float));
+    	  memcpy(&cloudOut.data[i * cloudOut.point_step + y_offset], &matrixPtr[i+length], sizeof(float));
+    	  memcpy(&cloudOut.data[i * cloudOut.point_step + z_offset], &matrixPtr[i+2*length], sizeof(float));
+      };
+    }
+
+
+    ros::NodeHandle n_;
+
+
+protected:
+    ros::Subscriber point_cloud_sub_;
+    ros::Publisher point_cloud_pub_;
+
+    TransformListener tf_listener_;
+    StampedTransform transform_old_;
+
+    pcl::PointCloud<pcl::PointXYZ> map_;
+
+    bool first_;
+
+};
+
+//#######################
+//#### main programm ####
+int main(int argc, char** argv)
+{
+	/// initialize ROS, specify name of node
+	ros::init(argc, argv, "point_cloud_filter");
+
+	/// Create a handle for this node, initialize node
+	ros::NodeHandle nh;
+
+	/// Create camera node class instance
+	AggregatePointMap aggregate_point_map(nh);
+
+	ros::spin();
+
+	return 0;
+}
+
---
+++ ros/src/point_cloud_filter.cpp
@@ -0,0 +1,221 @@
+/****************************************************************
+ *
+ * Copyright (c) 2010
+ *
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: care-o-bot
+ * ROS stack name: cob_vision
+ * ROS package name: cob_env_model
+ * Description:
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Author: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+ * Supervised by: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+ *
+ * Date of creation: 01/2011
+ * ToDo:
+ * Create ROS parameters for filter
+ * Write as nodelet
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+
+//##################
+//#### includes ####
+
+// standard includes
+//--
+
+// ROS includes
+#include <ros/ros.h>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+// ROS message includes
+#include <sensor_msgs/PointCloud2.h>
+
+// external includes
+#include <boost/timer.hpp>
+#include <cob_vision_utils/VisionUtils.h>
+
+
+//####################
+//#### node class ####
+class PointCloudFilter
+{
+public:
+    // Constructor
+	PointCloudFilter(const ros::NodeHandle& nh)
+	  :	n_(nh),
+	    filter_by_amplitude_(false),
+	    filter_tearoff_(false),
+	    filter_speckle_(false),
+	    filter_by_confidence_(false)
+	{
+		point_cloud_sub_ = n_.subscribe("point_cloud2", 1, &PointCloudFilter::PointCloudSubCallback, this);
+		point_cloud_pub_ = n_.advertise<sensor_msgs::PointCloud2>("point_cloud2_filtered",1);
+		filter_by_amplitude_ = true;
+	}
+
+
+    // Destructor
+    ~PointCloudFilter()
+    {
+    	/// void
+    }
+
+    void PointCloudSubCallback(const sensor_msgs::PointCloud2Ptr& pc)
+    {
+    	//ROS_INFO("PointCloudSubCallback");
+		cv::Mat xyz_mat_32F3 = cv::Mat(pc->height, pc->width, CV_32FC3);
+		cv::Mat intensity_mat_32F1 = cv::Mat(pc->height, pc->width, CV_32FC1);
+
+		//assumption: data order is always x->y->z in PC, datatype = float
+		int x_offset = 0, i_offset = 0;
+		for (size_t d = 0; d < pc->fields.size(); ++d)
+		{
+			if(pc->fields[d].name == "x")
+				x_offset = pc->fields[d].offset;
+			if(pc->fields[d].name == "intensity")
+				i_offset = pc->fields[d].offset;
+		}
+
+		float* f_ptr = 0;
+		float* i_ptr = 0;
+		int pc_msg_idx=0;
+		for (int row = 0; row < xyz_mat_32F3.rows; row++)
+		{
+			f_ptr = xyz_mat_32F3.ptr<float>(row);
+			i_ptr = intensity_mat_32F1.ptr<float>(row);
+			for (int col = 0; col < xyz_mat_32F3.cols; col++, pc_msg_idx++)
+			{
+				memcpy(&f_ptr[3*col], &pc->data[pc_msg_idx * pc->point_step + x_offset], 3*sizeof(float));
+				memcpy(&i_ptr[col], &pc->data[pc_msg_idx * pc->point_step + i_offset], sizeof(float));
+			}
+		}
+		if(filter_speckle_ || filter_by_amplitude_ || filter_tearoff_)
+		{
+			if(filter_by_amplitude_) FilterByAmplitude(xyz_mat_32F3, intensity_mat_32F1);
+			if(filter_speckle_) FilterSpeckles(xyz_mat_32F3);
+			if(filter_tearoff_) FilterTearOffEdges(xyz_mat_32F3);
+			pc_msg_idx=0;
+			for (int row = 0; row < xyz_mat_32F3.rows; row++)
+			{
+				f_ptr = xyz_mat_32F3.ptr<float>(row);
+				for (int col = 0; col < xyz_mat_32F3.cols; col++, pc_msg_idx++)
+				{
+					memcpy(&pc->data[pc_msg_idx * pc->point_step + x_offset], &f_ptr[3*col], 3*sizeof(float));
+				}
+			}
+		}
+		if(filter_by_confidence_) FilterByConfidence(pc);
+
+		point_cloud_pub_.publish(pc);
+    }
+
+    void FilterSpeckles(cv::Mat xyz_mat_32F3)
+    {
+    	cv::Mat buf;
+    	ipa_Utils::FilterSpeckles(xyz_mat_32F3, 20, 0.01, buf);
+    }
+
+    void FilterByAmplitude(cv::Mat xyz_mat_32F3, cv::Mat intensity_mat_32F1)
+    {
+    	ipa_Utils::FilterByAmplitude(xyz_mat_32F3, intensity_mat_32F1, 0, 0, 500, 60000);
+    }
+
+    void FilterTearOffEdges(cv::Mat xyz_mat_32F3)
+    {
+    	ipa_Utils::FilterTearOffEdges(xyz_mat_32F3, 0, 6);
+    }
+
+    void FilterByConfidence(const sensor_msgs::PointCloud2Ptr& pc)
+    {
+		//assumption: data order is always x->y->z in PC, datatype = float
+		int x_offset = 0, c_offset = 0;
+		for (size_t d = 0; d < pc->fields.size(); ++d)
+		{
+			if(pc->fields[d].name == "x")
+				x_offset = pc->fields[d].offset;
+			if(pc->fields[d].name == "confidence")
+				c_offset = pc->fields[d].offset;
+		}
+		float c_ptr;
+		float f_ptr[3];
+		f_ptr[0] = f_ptr[1] = f_ptr[2] = 0;
+		for (int pc_msg_idx = 0; pc_msg_idx < pc->height*pc->width; pc_msg_idx++)
+		{
+			memcpy(&c_ptr, &pc->data[pc_msg_idx * pc->point_step + c_offset], sizeof(float));
+			if(*(float*)&pc->data[pc_msg_idx * pc->point_step + c_offset] < 55000)
+			{
+				memcpy(&pc->data[pc_msg_idx * pc->point_step + x_offset], &f_ptr, 3*sizeof(float));
+			}
+		}
+    }
+
+    ros::NodeHandle n_;
+
+
+protected:
+    ros::Subscriber point_cloud_sub_;
+    ros::Publisher point_cloud_pub_;
+
+    bool filter_by_amplitude_;
+    bool filter_tearoff_;
+    bool filter_speckle_;
+    bool filter_by_confidence_;
+};
+
+//#######################
+//#### main programm ####
+int main(int argc, char** argv)
+{
+	/// initialize ROS, specify name of node
+	ros::init(argc, argv, "point_cloud_filter");
+
+	/// Create a handle for this node, initialize node
+	ros::NodeHandle nh;
+
+	/// Create camera node class instance
+	PointCloudFilter point_cloud_filter(nh);
+
+	ros::spin();
+
+	return 0;
+}
+
