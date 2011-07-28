051f704e48acbd771bd2026f077ba06ddb256b8a
ipa-goa <goa@ipa.fhg.de>
2011-07-28 18:01:09 +0200

    improved edge detection

--- common/src/tools/create_point_cloud.cpp
+++ common/src/tools/create_point_cloud.cpp
@@ -35,8 +35,8 @@
 	pcl::io::savePCDFileASCII ("/home/goa/pcl_daten/cloud_sim.pcd", cloud);
 }*/
 
-//Create corner
-int main(int argc, char** argv)
+//Create vertical corner
+/*int main(int argc, char** argv)
 {
 	pcl::PointCloud<pcl::PointXYZRGBNormal> cloud;
 	//cloud.points.resize(40000);
@@ -55,7 +55,7 @@ int main(int argc, char** argv)
 		{
 			if(x>=0)
 			{
-				x = 0;
+				x -= 0.009;
 				z -= 0.01;
 				n_z = 0;
 				n_x = -1;
@@ -76,4 +76,49 @@ int main(int argc, char** argv)
 		}
 	}
 	pcl::io::savePCDFileASCII ("/home/goa/pcl_daten/corner/corner_sim.pcd", cloud);
+}*/
+
+//create horizontal corner
+int main(int argc, char** argv)
+{
+	pcl::PointCloud<pcl::PointXYZRGBNormal> cloud;
+	//cloud.points.resize(40000);
+	cloud.height=20;
+	cloud.width=20;
+	int pt_ctr=0;
+	double x=-0.1, y=-0.1, z=1;
+	double n_x=0, n_y=0, n_z=0;
+	for(int i=0; i<cloud.height; i++, y+=0.01)
+	{
+		if(y>=0)
+		{
+			y -= 0.009;
+			z -= 0.01;
+			n_z = 0;
+			n_x = -1;
+		}
+		x=-0.1;
+		//z=1;
+		n_z = -1;
+		n_x = 0;
+		for(int j=0; j<cloud.width; j++, x+=0.01, pt_ctr++)
+		{
+			/*else
+				z += 0.01;*/
+
+			pcl::PointXYZRGBNormal p;
+			p.x = x;
+			p.y = y;
+			p.z = z;
+			p.rgb = 0;
+			p.normal_x = n_x;
+			p.normal_y = n_y;
+			p.normal_z = n_z;
+			p.curvature = 0;
+			//if(i<cloud.width/2) p.z=1.0;
+			//else p.z=1.2;
+			cloud.points.push_back(p);
+		}
+	}
+	pcl::io::savePCDFileASCII ("/home/goa/pcl_daten/corner/corner_sim_h.pcd", cloud);
 }
