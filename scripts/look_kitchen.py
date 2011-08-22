7a1c6535816b99a9416933540efc139c9180e781
ipa-goa <goa@ipa.fhg.de>
2011-08-22 10:45:50 +0200

    modified script

--- scripts/look_kitchen.py
+++ scripts/look_kitchen.py
@@ -29,18 +29,27 @@ class MyScript(script):
 
     def Run(self):
         rospy.loginfo("Running script...")
-     	self.sss.move("arm","overtray")
-        self.sss.move("torso","back")
+        #self.sss.init("base")
+        #self.sss.init("torso")
+        #self.sss.init("head")
+     	#self.sss.move("arm","overtray")
+        self.sss.move("torso","home")
+        self.sss.move("head","front")
+        self.sss.wait_for_input()
        # self.sss.move("torso","home")
-        self.sss.move("base",[-2.2, -1.6, 0])
+        self.sss.move("base",[-1.5, -1.6, 3.14], mode="omni")
+        self.sss.wait_for_input()
        # self.sss.move("torso","back")
        # self.sss.move("base",[-2, -1, 0])
       # self.sss.move("base",[-2, 0, 0])
 	  #  self.sss.move("torso","home")
    #     self.sss.move("base",[-2, 0, 0])
        # self.sss.move("base",[-2, 0, 0])
-        self.sss.move("base",[-2.2, 1.8, 0])
-        self.sss.move("base",[0, 0, 0])
+        self.sss.move("base",[-1.5, 1.6, 3.14],mode="linear")
+        self.sss.move("torso","front")
+        self.sss.move("base",[-1.5, -1.6, 3.14], mode="linear")
+        self.sss.wait_for_input()
+        self.sss.move("base",[0, 0, 0], mode="omni")
         
 
         
