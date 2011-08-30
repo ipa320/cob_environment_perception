ff359d23ff25e97e297f28489f235cbc2bcb564e
ipa-goa <goa@ipa.fhg.de>
2011-08-30 18:24:02 +0200

    added params to save files

--- ros/src/table_object_cluster_nodelet.cpp
+++ ros/src/table_object_cluster_nodelet.cpp
@@ -98,6 +98,8 @@ public:
   TableObjectClusterNodelet()
   : as_(0)
   {
+    save_to_file_ = false;
+    file_path_ = "";
   }
 
   // Destructor
@@ -117,6 +119,9 @@ public:
     get_bb_client_ = n_.serviceClient<cob_env_model_msgs::GetBoundingBoxes>("get_known_objects");
     as_= new actionlib::SimpleActionServer<cob_env_model_msgs::TableObjectClusterAction>(n_, "table_object_cluster", boost::bind(&TableObjectClusterNodelet::actionCallback, this, _1), false);
     as_->start();
+
+    n_.param("plane_extraction/file_path" ,file_path_ ,std::string("/home/goa/tmp/"));
+    n_.param("table_object_cluster/save_to_file" ,save_to_file_ ,false);
   }
 
 
@@ -142,16 +147,14 @@ public:
     }
     pcl::PointCloud<Point>::Ptr pc(new pcl::PointCloud<Point>);
     pcl::PointCloud<Point>::Ptr hull(new pcl::PointCloud<Point>);
-    ROS_INFO("Hull size: %d", srv.response.hull.width*srv.response.hull.height);
     pcl::fromROSMsg(srv.response.pc, *pc);
     pcl::fromROSMsg(srv.response.hull, *hull);
     ROS_INFO("Hull size: %d", hull->size());
-    pcl::io::savePCDFileASCII ("/home/goa/tmp/pc.pcd", *pc);
-    pcl::io::savePCDFileASCII ("/home/goa/tmp/hull.pcd", *hull);
+
 
     pcl::PointCloud<Point>::Ptr pc_roi(new pcl::PointCloud<Point>);
     toc.extractTableRoi(pc, hull, *pc_roi);
-    pcl::io::savePCDFileASCII ("/home/goa/tmp/table_roi.pcd", *pc_roi);
+
     std::vector<pcl::PointCloud<Point>, Eigen::aligned_allocator<pcl::PointCloud<Point> > > known_objs;
     for(unsigned int i=0; i<srv2.response.bounding_boxes.size(); i++)
     {
@@ -172,34 +175,42 @@ public:
     known_objs.push_back(obj);*/
     pcl::PointCloud<Point>::Ptr pc_roi_red(new pcl::PointCloud<Point>);
     toc.removeKnownObjects(pc_roi, known_objs, *pc_roi_red);
-    pcl::io::savePCDFileASCII ("/home/goa/tmp/table_roi_red.pcd", *pc_roi_red);
     std::vector<pcl::PointCloud<Point>, Eigen::aligned_allocator<pcl::PointCloud<Point> > > bounding_boxes;
     toc.calculateBoundingBoxes(pc_roi_red,bounding_boxes);
     for(unsigned int i=0; i< bounding_boxes.size(); i++)
     {
-      std::stringstream ss;
-      ss << "/home/goa/tmp/bb_" << i << ".pcd";
-      pcl::io::savePCDFileASCII (ss.str(), bounding_boxes[i]);
       sensor_msgs::PointCloud2 bb;
       pcl::toROSMsg(bounding_boxes[i], bb);
       result.bounding_boxes.push_back(bb);
     }
-    /*if(!lock)
-    //if(!lock.owns_lock())
+
+    if(save_to_file_)
     {
-      ROS_INFO(" actionCallback not owning lock");
-      return;
+      std::stringstream ss;
+      ss << file_path_ << "/pc.pcd";
+      pcl::io::savePCDFileASCII (ss.str(), *pc);
+      ss.str("");
+      ss.clear();
+      ss << file_path_ << "/hull.pcd";
+      pcl::io::savePCDFileASCII (ss.str(), *hull);
+      ss.str("");
+      ss.clear();
+      ss << file_path_ << "/table_roi.pcd";
+      pcl::io::savePCDFileASCII (ss.str(), *pc_roi);
+      ss.str("");
+      ss.clear();
+      ss << file_path_ << "/table_roi_red.pcd";
+      pcl::io::savePCDFileASCII (ss.str(), *pc_roi_red);
+      ss.str("");
+      ss.clear();
+      for(unsigned int i=0; i< bounding_boxes.size(); i++)
+      {
+        ss << "/home/goa/tmp/bb_" << i << ".pcd";
+        pcl::io::savePCDFileASCII (ss.str(), bounding_boxes[i]);
+        ss.str("");
+        ss.clear();
+      }
     }
-    else
-      ROS_INFO(" actionCallback owns lock");*/
-    /*feedback_.currentStep.data = std::string("plane extraction");
-    std::vector<pcl::PointCloud<Point> > v_cloud_hull;
-    std::vector<std::vector<pcl::Vertices> > v_hull_polygons;
-    std::vector<pcl::ModelCoefficients> v_coefficients_plane;
-    extractPlane(pc_cur_.makeShared(), v_cloud_hull, v_hull_polygons, v_coefficients_plane);
-    pcl::copyPointCloud(pc_cur_, pc_plane_);
-    // only save dominant plane
-    pcl::copyPointCloud(v_cloud_hull[0], hull_);*/
     as_->setSucceeded(result);
   }
 
@@ -211,58 +222,13 @@ protected:
   actionlib::SimpleActionServer<cob_env_model_msgs::TableObjectClusterAction>* as_;
   ros::ServiceClient get_plane_client_;
   ros::ServiceClient get_bb_client_;
-  // create messages that are used to published feedback/result
   boost::mutex mutex_;
 
   TableObjectCluster toc;
 
+  bool save_to_file_;
+  std::string file_path_;
+
 };
 
 PLUGINLIB_DECLARE_CLASS(cob_env_model, TableObjectClusterNodelet, TableObjectClusterNodelet, nodelet::Nodelet)
-
-/// Old code
-
-/*void publishMarker(pcl::PointCloud<Point>& cloud_hull, std::string& frame_id)
-{
-  visualization_msgs::Marker marker;
-  marker.action = visualization_msgs::Marker::ADD;
-  marker.type = visualization_msgs::Marker::TRIANGLE_LIST;
-  marker.lifetime = ros::Duration();
-  marker.header.frame_id = frame_id;
-  marker.id = ctr_;
-
-
-  //create the marker in the table reference frame
-  //the caller is responsible for setting the pose of the marker to match
-
-  marker.scale.x = 1;
-  marker.scale.y = 1;
-  marker.scale.z = 1;
-
-  geometry_msgs::Point pt1, pt2, pt3;
-  pt1.x = cloud_hull.points[0].x;
-  pt1.y = cloud_hull.points[0].y;
-  pt1.z = cloud_hull.points[0].z;
-
-  for(unsigned int i = 1; i < cloud_hull.points.size()-1; ++i)
-  {
-    pt2.x = cloud_hull.points[i].x;
-    pt2.y = cloud_hull.points[i].y;
-    pt2.z = cloud_hull.points[i].z;
-
-    pt3.x = cloud_hull.points[i+1].x;
-    pt3.y = cloud_hull.points[i+1].y;
-    pt3.z = cloud_hull.points[i+1].z;
-
-    marker.points.push_back(pt1);
-    marker.points.push_back(pt2);
-    marker.points.push_back(pt3);
-  }
-
-  marker.color.r = 0;
-  marker.color.g = 0;
-  marker.color.b = 1;
-  marker.color.a = 1.0;
-
-  table_marker_pub_.publish(marker);
-}*/
