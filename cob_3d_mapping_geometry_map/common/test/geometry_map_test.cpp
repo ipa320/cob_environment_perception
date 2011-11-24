c7fc1d1f0236a43ec86bcae86a4b9206421fcaac
ipa-goa-hh <heiko.hoennige@googlemail.com>
2011-11-24 16:06:37 +0100

    modified:   cob_3d_mapping_geometry_map/common/include/cob_3d_mapping_geometry_map/geometry_map.h 	modified:   cob_3d_mapping_geometry_map/common/include/cob_3d_mapping_geometry_map/test/geometry_map_test.h 	modified:   cob_3d_mapping_geometry_map/common/src/geometry_map.cpp 	modified:   cob_3d_mapping_geometry_map/common/test/geometry_map_test.cpp

--- cob_3d_mapping_geometry_map/common/test/geometry_map_test.cpp
+++ cob_3d_mapping_geometry_map/common/test/geometry_map_test.cpp
@@ -38,6 +38,35 @@ GeometryMapTest::~GeometryMapTest() {}
 		gmv.showMap(gm.getMap());
 	}
 
+	void GeometryMapTest::curve()
+	{
+		MapEntryPtr m_p = MapEntryPtr(new MapEntry());
+		MapEntryPtr m_p2 = MapEntryPtr(new MapEntry());
+		MapEntryPtr m_p3 = MapEntryPtr(new MapEntry());
+		MapEntryPtr m_p4 = MapEntryPtr(new MapEntry());
+		MapEntryPtr m_p5 = MapEntryPtr(new MapEntry());
+
+		tp.rotate(m_p,0,0,25);
+		tp.rotate(m_p2,-0.75*sin(30*M_PI / 180),0.75*cos(30*M_PI / 180),12);
+		tp.rotate(m_p3,-0.75*sin(30*M_PI / 180)-0.75*sin(15*M_PI / 180),0.75*cos(30*M_PI / 180)+0.75*cos(15*M_PI / 180),0);
+		tp.rotate(m_p4,-0.75*sin(30*M_PI / 180)-0.75*sin(15*M_PI / 180),0.75*cos(30*M_PI / 180)+0.75*cos(15*M_PI / 180)+0.75,-12);
+		tp.rotate(m_p5,-0.75*sin(30*M_PI / 180),0.75*cos(30*M_PI / 180)+2*0.75*cos(15*M_PI / 180)+0.75,-25);
+
+		std::cout << "m_p2 d " << m_p2->d << " normal " <<std::endl << m_p2->normal << std::endl;
+		std::cout << "m_p3 d " << m_p3->d << " normal " <<std::endl<< m_p3->normal << std::endl;
+		std::cout << "dot " << m_p2->normal.dot(m_p3->normal) << std::endl;
+
+		gm.addMapEntry(m_p);
+		gm.addMapEntry(m_p2);
+		gm.addMapEntry(m_p3);
+		gm.addMapEntry(m_p4);
+		gm.addMapEntry(m_p5);
+
+		gmv.showMap(gm.getMap());
+
+
+
+	}
 
 
 	void GeometryMapTest::box()
@@ -75,7 +104,7 @@ GeometryMapTest::~GeometryMapTest() {}
 			tp.rotate(m_p,0,0,125);
 			gm.addMapEntry(m_p);
 			//gmv.showMap(gm.getMap());
-			tp.rotate(m_p2,0,0,143.4);                                   // intersection wenn winkel kleiner wie 18.5crad
+			tp.rotate(m_p2,0,0,140);                                   // intersection wenn winkel kleiner wie 18.5crad
 			gm.addMapEntry(m_p2);
 			gmv.showMap(gm.getMap());
 
@@ -116,6 +145,29 @@ GeometryMapTest::~GeometryMapTest() {}
 
 		gmv.showMap(gm.getMap());
 	}
+	void GeometryMapTest::easyplane()
+	{
+		  MapEntryPtr m_p = MapEntryPtr(new MapEntry());
+
+		  m_p->id = 0;
+		  m_p->normal << 0,0,1;
+		  m_p->d = -1;
+		  std::vector<Eigen::Vector3f> vv;
+		  Eigen::Vector3f v;
+		  v << 1,0,1;
+		  vv.push_back(v);
+		  v << 1,1,1;
+		  vv.push_back(v);
+		  v << 0,1,1;
+		  vv.push_back(v);
+		  v << 0,0,1;
+		  vv.push_back(v);
+		  m_p->polygon_world.push_back(vv);
+		  gm.addMapEntry(m_p);
+
+		  gmv.showMap(gm.getMap());
+
+	}
 
 
 
@@ -123,11 +175,13 @@ int main (int argc, char** argv)
 {
 
 	GeometryMapTest gmt;
-	gmt.testnormaldirection();
-	//gmt.box();
+	//gmt.testnormaldirection();
+	gmt.box();
 	//gmt.fillingHole();
-	//gmt.rotate();
+//	gmt.rotate();
 	//gmt.strongMerged();
+	//gmt.curve();
+	//gmt.easyplane();
 //	TestPlanes tp;
 //	GeometryMapVisualisation gmv;
 //	GeometryMap gm;
