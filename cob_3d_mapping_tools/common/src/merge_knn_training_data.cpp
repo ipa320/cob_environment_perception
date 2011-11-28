60a8bbccf00597525bf9d8e67a7f1f3a0f03b6b4
ipa-goa-sf <fuchs_steffen@gmx.net>
2011-11-28 17:41:27 +0100

    added doxygen comments

--- cob_3d_mapping_tools/common/src/merge_knn_training_data.cpp
+++ cob_3d_mapping_tools/common/src/merge_knn_training_data.cpp
@@ -1,3 +1,61 @@
+/****************************************************************
+ *
+ * Copyright (c) 2011
+ *
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: care-o-bot
+ * ROS stack name: cob_environment_perception_intern
+ * ROS package name: cob_3d_mapping_tools
+ * Description:
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Author: Steffen Fuchs, email:georg.arbeiter@ipa.fhg.de
+ * Supervised by: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+ *
+ * Date of creation: 11/2011
+ * ToDo:
+ *
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+/*! script merges fpfh PCD file from multiple source files into single one
+ * and generates a label reference file for the knn classifier.
+ */
+
 #include <boost/program_options.hpp>
 #include <fstream>
 
@@ -35,6 +93,16 @@ void readOptions(int argc, char* argv[])
 
   if (vm.count("help") || !vm.count("in"))
   {
+    cout << "script merges fpfh PCD file from multiple source files into single one "
+	 << "and generates a label reference file for the knn classifier." << endl;
+    cout << "Searches for files containing the strings:" << endl;
+    cout << "\t  \"plane\"" << endl;
+    cout << "\t  \"edge_convex_\"" << endl;
+    cout << "\t  \"edge_concave_\"" << endl;
+    cout << "\t  \"sphere_convex_\"" << endl;
+    cout << "\t  \"sphere_concave_\"" << endl;
+    cout << "\t  \"cylinder_convex_\"" << endl;
+    cout << "\t  \"cylinder_concave_\"" << endl;
     cout << cmd_line << endl;
     exit(0);
   }
