7ead81312a9dc6550dc46cab53ef74bad80cd43e
ipa-goa-sf <fuchs_steffen@gmx.net>
2011-11-28 17:41:27 +0100

    added doxygen comments

--- cob_3d_mapping_tools/common/src/fpfh_primitives.cpp
+++ cob_3d_mapping_tools/common/src/fpfh_primitives.cpp
@@ -1,9 +1,63 @@
-/*
- * Generates FPFH of a single point on various shapes
- * TODO: For some shape sizes this point not always lies in the center of the shape
+/****************************************************************
+ *
+ * Copyright (c) 2011
+ *
+ * Fraunhofer Institute for Manufacturing Engineering
+ * and Automation (IPA)
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Project name: care-o-bot
+ * ROS stack name: cob_environment_perception_intern
+ * ROS package name: cob_3d_mapping_tools
+ * Description:
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Author: Steffen Fuchs, email:georg.arbeiter@ipa.fhg.de
+ * Supervised by: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+ *
+ * Date of creation: 11/2011
+ * ToDo:
+ *
+ *
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
+/*!
+ * @brief Generates FPFH of a single point on various shapes
+ * @todo For some shape sizes this point not always lies in the center of the shape
  */
 
-#include <cob_3d_mapping_tools/point_generator.h>
+#include "cob_3d_mapping_tools/point_generator.h"
 #include <cob_3d_mapping_common/label_defines.h>
 
 #include <pcl/io/pcd_io.h>
@@ -70,6 +124,8 @@ void readOptions(int argc, char* argv[])
 
   if (vm.count("help") || !vm.count("in"))
   {
+    cout << "Generates FPFH of a single point on various shapes" << endl;
+    cout << "TODO: For some shape sizes the point not always lies in the center of the shape" << endl;
     cout << cmd_line << endl;
     exit(0);
   }
