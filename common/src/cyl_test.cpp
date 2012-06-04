bfd09a3e15c890cbad6f3f928b0d216477d1ce4b
Thomas Zwoelfer <t.zwoelfer@gmx.de>
2012-06-04 11:55:54 +0200

    still error in merge

--- common/src/cyl_test.cpp
+++ common/src/cyl_test.cpp
@@ -11,283 +11,191 @@
 using namespace cob_3d_mapping;
 
 
+
+void
+transform_cylinder(CylinderPtr & c_ptr,Eigen::Affine3f& trafo)
+{
+
+
+	Cylinder & c=*c_ptr;
+
+ 	 for (int i = 0; i < (int) c.contours.size(); ++i) {
+ 		 for (int j = 0; j < (int) c.contours[i].size(); ++j) {
+
+
+ 			 c.contours[i][j]=trafo*c.contours[i][j];
+
+
+ 		}
+ 	}
+
+c.origin_=trafo*c.origin_;
+std::cout<<"origin\n"<<c.origin_<<std::endl;
+
+for (int i = 0; i < 3; ++i) {
+
+	c.axes_[i]=trafo.rotation()*c.axes_[i];
+	std::cout<<"axis -"<<i<<" \n"<<c.axes_[i]<<std::endl;
+}
+
+
+float roll,pitch,yaw,x,y,z;
+pcl::getTranslationAndEulerAngles(trafo,x,y,z,roll,pitch,yaw);
+
+
+	std::cout<<" x= "<<x<<" y= "<<z<<" z= "<<z<<" roll= "<<roll<<" pitch= "<<pitch<<" yaw= "<<yaw<<std::endl;
+}
+
 int main(int argc, char **argv) {
 
 	std::cout <<"goa-tz --> start"<< std::endl;
 
 
-////####################################################
-////Cylinder #1
-//
-//CylinderPtr  c1  =CylinderPtr(new Cylinder());
-//
-//c1->id = 0;
-//
-//
-//
-//Eigen::Vector3f x_axis1,y_axis1,z_axis1;
-//std::vector<Eigen::Vector3f> axes1;
-//Eigen::Vector3f origin1;
-// std::vector<Eigen::Vector3f> contour1;
-// Eigen::Vector3f v1;
-//
-//
-//x_axis1 << 1,0,0;
-//axes1.push_back(x_axis1);
-//
-//y_axis1 << 0,1,0;
-//axes1.push_back(y_axis1);
-//
-//z_axis1 << 0,0,1;
-//axes1.push_back(z_axis1);
-//
-//c1->axes_=axes1;
-//
-//
-//
-// v1 << -1, 0, 1;
-// contour1.push_back(v1);
-// v1 << 1, 0, 1;
-// contour1.push_back(v1);
-// v1 << 1 ,0 ,-1;
-// contour1.push_back(v1);
-// v1 << -1, 0 ,-1;
-// contour1.push_back(v1);
-// c1->contours.push_back(contour1);
-//
-//
-//c1->merged=1;
-// origin1 << 0,0,0;
-// c1->origin_=origin1;
-//
-// c1->r_=1;
-//
-//
-//
-// c1->holes.push_back(0);
-// c1->debug_=false;
-// c1->unroll();
-//
-// if (c1->debug_== true) {
-//
-// 	 std::cout<<"goa-tz C1--> new vertices"<<std::endl;
-// 	 for (int i = 0; i < (int) c1->unrolled_.contours.size(); ++i) {
-// 		 for (int j = 0; j < (int) c1->unrolled_.contours[i].size(); ++j) {
-//
-// 			 std::cout<<"contor "<<i<<" vertex "<<j <<"  : "<<std::endl<<"~~~~"<<std::endl<<c1->unrolled_.contours[i][j]<<std::endl<<std::endl;
-// 		}
-// 	}
-// }
-//
-//
-//
-//
-//
-//
-////####################################################
-////Cylinder  #2
-// CylinderPtr  c2  =CylinderPtr(new Cylinder());
-//
-//
-// c2->id = 0;
-//
-// Eigen::Vector3f x_axis2,y_axis2,z_axis2;
-// std::vector<Eigen::Vector3f> axes2;
-// Eigen::Vector3f origin2;
-//  std::vector<Eigen::Vector3f> contour2;
-//  Eigen::Vector3f v2;
-//
-//
-// x_axis2 << 0,-1,0;
-// axes2.push_back(x_axis2);
-//
-// y_axis2 << 1,0,0;
-// axes2.push_back(y_axis2);
-//
-// z_axis2 << 0,0,1;
-// axes2.push_back(z_axis2);
-//
-// c2->axes_=axes2;
-//
-//
-//
-//  v2 << 0, 1, 1;
-//  contour2.push_back(v2);
-//  v2 << 0, -1, 1;
-//  contour2.push_back(v2);
-//  v2 << 0 ,-1 ,-1;
-//  contour2.push_back(v2);
-//  v2 << 0, 1 ,-1;
-//  contour2.push_back(v2);
-//  c2->contours.push_back(contour2);
-//
-//
-//c2->merged=1;
-//  origin2 << 0,0,0;
-//  c2->origin_=origin2;
-//
-//  c2->r_=1;
-//
-//
-//
-//  c2->holes.push_back(0);
-//  c2->debug_=false;
-//  c2->unroll();
-//
-//  if (c2->debug_== true) {
-//
-//  	 std::cout<<"goa-tz C2--> new vertices"<<std::endl;
-//  	 for (int i = 0; i < (int) c2->unrolled_.contours.size(); ++i) {
-//  		 for (int j = 0; j < (int) c2->unrolled_.contours[i].size(); ++j) {
-//
-//  			 std::cout<<"contor "<<i<<" vertex "<<j <<"  : "<<std::endl<<"~~~~"<<std::endl<<c2->unrolled_.contours[i][j]<<std::endl<<std::endl;
-//  		}
-//  	}
-//  }
-	//~~Cylinder 1~
-	CylinderPtr  c1  =CylinderPtr(new Cylinder());
-	Eigen::Vector3f x_axis1,y_axis1,z_axis1;
-	std::vector<Eigen::Vector3f> axes1;
-	Eigen::Vector3f origin1;
-	 std::vector<Eigen::Vector3f> contour1;
-	 Eigen::Vector3f v1;
-	x_axis1 << -0.998931,0.046236,-0.000000;
-	axes1.push_back(x_axis1);
-	y_axis1 << -0.046236,-0.998931,0.000000;
-	axes1.push_back(y_axis1);
-	c1->id = 0;
-	z_axis1 << 0.000000,0.000000,1.000000;
-	axes1.push_back(z_axis1);
-	c1->axes_=axes1;
-	 v1 << -0.339341, 0.368494, 0.080342;
-	 contour1.push_back(v1);
-	 c1->contours.push_back(contour1);
-	 origin1 << 0.000000,0.000000,0.000000;
-	 c1->origin_=origin1;
-	 c1->r_=0.500940;
-	 c1->holes.push_back(0);
-	 c1->debug_=false;
-	 c1->merged=1;
+//####################################################
+//Cylinder #1
+
+CylinderPtr  c1  =CylinderPtr(new Cylinder());
+
+c1->id = 0;
 
 
-	 v1 << -0.471480, 0.169255, 0.592080;
-	 contour1.push_back(v1);
-	 c1->contours.push_back(contour1);
-	 origin1 << 0.000000,0.000000,0.000000;
-	 c1->origin_=origin1;
-	 c1->r_=0.500940;
-	 c1->holes.push_back(0);
-	 c1->debug_=false;
-	 c1->merged=1;
+
+Eigen::Vector3f x_axis1,y_axis1,z_axis1;
+std::vector<Eigen::Vector3f> axes1;
+Eigen::Vector3f origin1;
+ std::vector<Eigen::Vector3f> contour1;
+ Eigen::Vector3f v1;
 
 
-	 v1 << 0.012288, -0.500789, 0.623528;
-	 contour1.push_back(v1);
-	 c1->contours.push_back(contour1);
-	 origin1 << 0.000000,0.000000,0.000000;
-	 c1->origin_=origin1;
-	 c1->r_=0.500940;
-	 c1->holes.push_back(0);
-	 c1->debug_=false;
-	 c1->merged=1;
+x_axis1 << 1,0,0;
+axes1.push_back(x_axis1);
+
+y_axis1 << 0,1,0;
+axes1.push_back(y_axis1);
+
+z_axis1 << 0,0,1;
+axes1.push_back(z_axis1);
+
+c1->axes_=axes1;
 
 
-	//~~Cylinder 2~
-	CylinderPtr  c2  =CylinderPtr(new Cylinder());
-	Eigen::Vector3f x_axis2,y_axis2,z_axis2;
-	std::vector<Eigen::Vector3f> axes2;
-	Eigen::Vector3f origin2;
-	 std::vector<Eigen::Vector3f> contour2;
-	 Eigen::Vector3f v2;
-	x_axis2 << -0.436181,0.899859,-0.000000;
-	axes2.push_back(x_axis2);
-	y_axis2 << -0.899859,-0.436181,0.000000;
-	axes2.push_back(y_axis2);
-	c2->id = 0;
-	z_axis2 << 0.000000,0.000000,1.000000;
-	axes2.push_back(z_axis2);
-	c2->axes_=axes2;
-	 v2 << 0.432329, 0.253046, 0.234369;
-	 contour2.push_back(v2);
-	 c2->contours.push_back(contour2);
-	 origin2 << 0.000000,0.000000,0.000000;
-	 c2->origin_=origin2;
-	 c2->r_=0.500940;
-	 c2->holes.push_back(0);
-	 c2->debug_=false;
-	 c2->merged=1;
+
+ v1 << -1, 0, 1;
+ contour1.push_back(v1);
+ v1 << 1, 0, 1;
+ contour1.push_back(v1);
+ v1 << 1 ,0 ,-1;
+ contour1.push_back(v1);
+ v1 << -1, 0 ,-1;
+ contour1.push_back(v1);
+ c1->contours.push_back(contour1);
 
 
-	 v2 << -0.498244, 0.051901, 0.996582;
-	 contour2.push_back(v2);
-	 c2->contours.push_back(contour2);
-	 origin2 << 0.000000,0.000000,0.000000;
-	 c2->origin_=origin2;
-	 c2->r_=0.500940;
-	 c2->holes.push_back(0);
-	 c2->debug_=false;
-	 c2->merged=1;
+c1->merged=1;
+ origin1 << 0,0,0;
+ c1->origin_=origin1;
+
+ c1->r_=1;
 
 
-	 v2 << -0.282440, 0.413724, 0.321632;
-	 contour2.push_back(v2);
-	 c2->contours.push_back(contour2);
-	 origin2 << 0.000000,0.000000,0.000000;
-	 c2->origin_=origin2;
-	 c2->r_=0.500940;
-	 c2->holes.push_back(0);
-	 c2->debug_=false;
-	 c2->merged=1;
+
+ c1->holes.push_back(0);
+ c1->debug_=false;
+
+ if (c1->debug_== true) {
+
+ 	 std::cout<<"goa-tz C1--> new vertices"<<std::endl;
+ 	 for (int i = 0; i < (int) c1->unrolled_.contours.size(); ++i) {
+ 		 for (int j = 0; j < (int) c1->unrolled_.contours[i].size(); ++j) {
+
+ 			 std::cout<<"contor "<<i<<" vertex "<<j <<"  : "<<std::endl<<"~~~~"<<std::endl<<c1->unrolled_.contours[i][j]<<std::endl<<std::endl;
+ 		}
+ 	}
+ }
 
 
-	//~~Cylinder 3~
-	CylinderPtr  c3  =CylinderPtr(new Cylinder());
-	Eigen::Vector3f x_axis3,y_axis3,z_axis3;
-	std::vector<Eigen::Vector3f> axes3;
-	Eigen::Vector3f origin3;
-	 std::vector<Eigen::Vector3f> contour3;
-	 Eigen::Vector3f v3;
-	x_axis3 << 0.695692,-0.718340,-0.000000;
-	axes3.push_back(x_axis3);
-	y_axis3 << 0.718340,0.695692,-0.000000;
-	axes3.push_back(y_axis3);
-	c3->id = 0;
-	z_axis3 << 0.000000,0.000000,1.000000;
-	axes3.push_back(z_axis3);
-	c3->axes_=axes3;
-	 v3 << 0.421749, -0.270312, 0.281687;
-	 contour3.push_back(v3);
-	 c3->contours.push_back(contour3);
-	 origin3 << 0.000000,0.000000,0.000000;
-	 c3->origin_=origin3;
-	 c3->r_=0.500940;
-	 c3->holes.push_back(0);
-	 c3->debug_=false;
-	 c3->merged=1;
 
 
-	 v3 << 0.500630, -0.017605, 0.176316;
-	 contour3.push_back(v3);
-	 c3->contours.push_back(contour3);
-	 origin3 << 0.000000,0.000000,0.000000;
-	 c3->origin_=origin3;
-	 c3->r_=0.500940;
-	 c3->holes.push_back(0);
-	 c3->debug_=false;
-	 c3->merged=1;
 
 
-	 v3 << -0.197825, -0.460224, 0.517959;
-	 contour3.push_back(v3);
-	 c3->contours.push_back(contour3);
-	 origin3 << 0.000000,0.000000,0.000000;
-	 c3->origin_=origin3;
-	 c3->r_=0.500940;
-	 c3->holes.push_back(0);
-	 c3->debug_=false;
-	 c3->merged=1;
+//####################################################
+//Cylinder  #2
+ CylinderPtr  c2  =CylinderPtr(new Cylinder());
 
 
+ c2->id = 0;
+
+ Eigen::Vector3f x_axis2,y_axis2,z_axis2;
+ std::vector<Eigen::Vector3f> axes2;
+ Eigen::Vector3f origin2;
+  std::vector<Eigen::Vector3f> contour2;
+  Eigen::Vector3f v2;
+
+
+ x_axis2 << 0,-1,0;
+ axes2.push_back(x_axis2);
+
+ y_axis2 << 1,0,0;
+ axes2.push_back(y_axis2);
+
+ z_axis2 << 0,0,1;
+ axes2.push_back(z_axis2);
+
+ c2->axes_=axes2;
+
+
+
+  v2 << 0, 1, 1;
+  contour2.push_back(v2);
+  v2 << 0, -1, 1;
+  contour2.push_back(v2);
+  v2 << 0 ,-1 ,-1;
+  contour2.push_back(v2);
+  v2 << 0, 1 ,-1;
+  contour2.push_back(v2);
+  c2->contours.push_back(contour2);
+
+
+c2->merged=1;
+  origin2 << 0,0,0;
+  c2->origin_=origin2;
+
+  c2->r_=1;
+
+
+
+  c2->holes.push_back(0);
+  c2->debug_=false;
+
+  if (c2->debug_== true) {
+
+  	 std::cout<<"goa-tz C2--> new vertices"<<std::endl;
+  	 for (int i = 0; i < (int) c2->unrolled_.contours.size(); ++i) {
+  		 for (int j = 0; j < (int) c2->unrolled_.contours[i].size(); ++j) {
+
+  			 std::cout<<"contor "<<i<<" vertex "<<j <<"  : "<<std::endl<<"~~~~"<<std::endl<<c2->unrolled_.contours[i][j]<<std::endl<<std::endl;
+  		}
+  	}
+  }
+
+
+//transform cylinders
+
+  float x,y,z,roll,pitch,yaw;
+
+  x=1;
+  y=1;
+  z=1;
+  roll=0.3;
+  pitch=0.4;
+  yaw=1;
+
+
+  Eigen::Affine3f trafo;
+  pcl::getTransformation(x,y,z,roll,pitch,yaw,trafo);
+
+transform_cylinder(c1,trafo);
+transform_cylinder(c2,trafo);
 
 
 //####################################################
@@ -303,7 +211,7 @@ limits.angle_thresh=0.97;
 limits.d_thresh= 0.1;
 
 //c_map.push_back(c2);
-c_map.push_back(c3);
+//c_map.push_back(c1);
 c_map.push_back(c2);
 
 
