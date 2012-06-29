187d6a50463ca157a4d30863fe3f9d82cfd48c8f
ipa-goa-wt <waqas.informatik@googlemail.com>
2012-06-29 12:58:52 +0200

    added new shapes for testing

--- cob_3d_mapping_common/ros/test/test_shape_array.cpp
+++ cob_3d_mapping_common/ros/test/test_shape_array.cpp
@@ -7,11 +7,11 @@
 #include <pcl/kdtree/kdtree_flann.h>
 #include <pcl/io/pcd_io.h>
 
-
 #include <sensor_msgs/PointCloud2.h>
 
 #include <cob_3d_mapping_msgs/ShapeArray.h>
 #include <cob_3d_mapping_msgs/Shape.h>
+#include <cob_3d_mapping_common/polygon.h>
 
 #include <ros/time.h>
 #include <math.h>
@@ -21,23 +21,346 @@
 #define PI 3.14159265
 using namespace std;
 
-float
-toDegrees (float radians)
+void firstShape(cob_3d_mapping_msgs::Shape& s)
 {
-  return radians * (180 / PI);
+  pcl::PointXYZ pt;
+  pcl::PointCloud<pcl::PointXYZ> pc;
+  sensor_msgs::PointCloud2 pc2;
+
+  //first shape
+  s.params[0] = 0;
+  s.params[1] = 0;
+  s.params[2] = 1;
+  s.params[3] = 0;
+
+  s.color.r = 1;
+  s.color.g = 0;
+  s.color.b = 0;
+  s.color.a = 1;
+
+  pt.x = 0.0;
+  pt.y = 0.0;
+  pt.z = 1.0;
+  pc.push_back (pt);
+
+  pt.x = 1.0;
+  pt.y = 0.0;
+  pt.z = 1.0;
+  pc.push_back (pt);
+
+  pt.x = 1.0;
+  pt.y = 1.0;
+  pt.z = 1.0;
+  pc.push_back (pt);
+
+  pt.x = 0.0;
+  pt.y = 1.0;
+  pt.z = 1.0;
+  pc.push_back (pt);
+
+  /*
+   pt.x = 0.0;
+   pt.y = 0.0;
+   pt.z = 1.0;
+   pc.push_back (pt);
+   */
+  Eigen::VectorXf centroid;
+  pcl::computeNDCentroid (pc, centroid);
+  //std::cout<<" centroid : "<<centroid<<"--> X: "<<centroid[0]<<"--> Y: "<<centroid[1]<<"--> Z: "<<centroid[2]<<std::endl;
+
+  s.centroid.x = centroid[0];
+  s.centroid.y = centroid[1];
+  s.centroid.z = centroid[2];
+
+  pcl::toROSMsg (pc, pc2);
+  s.points.push_back (pc2);
 }
 
-float
-toRadians (float degrees)
+void secondShape(cob_3d_mapping_msgs::Shape& s)
 {
-  return degrees * (PI / 180);
+  pcl::PointXYZ pt;
+  pcl::PointCloud<pcl::PointXYZ> pc;
+  sensor_msgs::PointCloud2 pc2;
+
+  //second shape
+  s.params[0] = 0;
+  s.params[1] = 0;
+  s.params[2] = 1;
+  s.params[3] = 0;
+/*
+  s.centroid.x = 1;
+  s.centroid.y = 1;
+  s.centroid.z = -1;
+*/
+  s.color.r = 0;
+  s.color.g = 1;
+  s.color.b = 0;
+  s.color.a = 1;
+/*
+  pc.clear ();
+  pc2.data.clear ();
+  */
+
+  pt.x = 0;
+  pt.y = 0;
+  pt.z = -1;
+  pc.push_back (pt);
+
+  pt.x = 2;
+  pt.y = 0;
+  pt.z = -1;
+  pc.push_back (pt);
+
+  pt.x = 2;
+  pt.y = 2;
+  pt.z = -1;
+  pc.push_back (pt);
+
+  pt.x = 0;
+  pt.y = 2;
+  pt.z = 1;
+  pc.push_back (pt);
+
+  /*
+   pt.x = 2;
+   pt.y = 2;
+   pt.z = 2;
+   pc.push_back (pt);
+   */
+  Eigen::VectorXf centroid;
+  pcl::computeNDCentroid (pc, centroid);
+  //std::cout<<" centroid : "<<centroid<<"--> X: "<<centroid[0]<<"--> Y: "<<centroid[1]<<"--> Z: "<<centroid[2]<<std::endl;
+
+  s.centroid.x = centroid[0];
+  s.centroid.y = centroid[1];
+  s.centroid.z = centroid[2];
+
+  pcl::toROSMsg (pc, pc2);
+  s.points.push_back (pc2);
+}
+
+void thirdShape(cob_3d_mapping_msgs::Shape& s)
+{
+  //third shape
+/*
+  s.centroid.x = -16 / 3;
+  s.centroid.y = -11 / 3;
+  s.centroid.z = -2;
+*/
+  pcl::PointXYZ pt;
+  pcl::PointCloud<pcl::PointXYZ> pc;
+  sensor_msgs::PointCloud2 pc2;
+
+  s.params[0] = 0;
+  s.params[1] = 0;
+  s.params[2] = -1;
+  s.params[3] = 0;
+
+  s.color.r = 0;
+  s.color.g = 0;
+  s.color.b = 1;
+  s.color.a = 1;
+
+  pt.x = -5;
+  pt.y = -5;
+  pt.z = -2;
+  pc.push_back (pt);
+
+  pt.x = -5;
+  pt.y = -1;
+  pt.z = -2;
+  pc.push_back (pt);
+
+  pt.x = -6;
+  pt.y = -5;
+  pt.z = -2;
+  pc.push_back (pt);
+
+  Eigen::VectorXf centroid;
+  pcl::computeNDCentroid (pc, centroid);
+  //std::cout<<" centroid : "<<centroid<<"--> X: "<<centroid[0]<<"--> Y: "<<centroid[1]<<"--> Z: "<<centroid[2]<<std::endl;
+
+  s.centroid.x = centroid[0];
+  s.centroid.y = centroid[1];
+  s.centroid.z = centroid[2];
+
+  pcl::toROSMsg (pc, pc2);
+  s.points.push_back (pc2);
+}
+
+void fourthShape(cob_3d_mapping_msgs::Shape& s)
+{
+  pcl::PointXYZ pt;
+  pcl::PointCloud<pcl::PointXYZ> pc;
+  sensor_msgs::PointCloud2 pc2;
+
+  //fourth shape
+  /*
+  s.centroid.x = -2 / 3;
+  s.centroid.y = -2 / 3;
+  s.centroid.z = 0;
+*/
+  s.params[0] = 0;
+  s.params[1] = 0;
+  s.params[2] = 1;
+  s.params[3] = 0;
+
+  s.color.r = 1;
+  s.color.g = 0;
+  s.color.b = 1;
+  s.color.a = 1;
+
+  pt.x = -1;
+  pt.y = 0;
+  pt.z = 0;
+  pc.push_back (pt);
+
+  pt.x = -1;
+  pt.y = -1;
+  pt.z = 0;
+  pc.push_back (pt);
+
+  pt.x = 0;
+  pt.y = -1;
+  pt.z = 0;
+  pc.push_back (pt);
+
+  /*
+   pt.x = 0;
+   pt.y = -2;
+   pt.z = 0;
+   pc.push_back (pt);
+   */
+
+  Eigen::VectorXf centroid;
+  pcl::computeNDCentroid (pc, centroid);
+  //std::cout<<" centroid : "<<centroid<<"--> X: "<<centroid[0]<<"--> Y: "<<centroid[1]<<"--> Z: "<<centroid[2]<<std::endl;
+
+  s.centroid.x = centroid[0];
+  s.centroid.y = centroid[1];
+  s.centroid.z = centroid[2];
+
+  pcl::toROSMsg (pc, pc2);
+  s.points.push_back (pc2);
+
+
+}
+
+void fifthShape(cob_3d_mapping_msgs::Shape& s)
+{
+  pcl::PointXYZ pt;
+  pcl::PointCloud<pcl::PointXYZ> pc;
+  sensor_msgs::PointCloud2 pc2;
+
+  //fifth shape
+  s.params[0] = 0;
+  s.params[1] = 0;
+  s.params[2] = -1;
+  s.params[3] = 0;
+
+/*
+  s.centroid.x = -2;
+  s.centroid.y = -1;
+  s.centroid.z = -3;
+*/
+
+  s.color.r = 1;
+  s.color.g = 1;
+  s.color.b = 0;
+  s.color.a = 1;
+
+  pt.x = 0.0;
+  pt.y = 0.0;
+  pt.z = -3.0;
+  pc.push_back (pt);
+
+  pt.x = -3.0;
+  pt.y = 0.0;
+  pt.z = -3.0;
+  pc.push_back (pt);
+
+  pt.x = -3.0;
+  pt.y = -3.0;
+  pt.z = -3.0;
+  pc.push_back (pt);
+
+
+  /*
+   pt.x = 0.0;
+   pt.y = -3.0;
+   pt.z = -3.0;
+   pc.push_back (pt);
+   */
+  Eigen::VectorXf centroid;
+  pcl::computeNDCentroid (pc, centroid);
+  //std::cout<<" centroid : "<<centroid<<"--> X: "<<centroid[0]<<"--> Y: "<<centroid[1]<<"--> Z: "<<centroid[2]<<std::endl;
+
+  s.centroid.x = centroid[0];
+  s.centroid.y = centroid[1];
+  s.centroid.z = centroid[2];
+
+  pcl::toROSMsg (pc, pc2);
+  s.points.push_back (pc2);
+
+}
+
+void sixthShape(cob_3d_mapping_msgs::Shape& s)
+{
+
+  pcl::PointXYZ pt;
+  pcl::PointCloud<pcl::PointXYZ> pc;
+  sensor_msgs::PointCloud2 pc2;
+
+
+  Eigen::Vector3f v1 (0,0,1),v2 (1,0,0), v3(0,0,0);
+  cout<< " v1 : "<<v1<<endl;
+  v1.normalize();
+  //cout<< " v1 norm : "<<v1<<endl;
+  v2.normalize();
+  v3 = v1.cross(v2);
+  cout<< " cross product : "<<v3<<endl;
+
+
+
+  //sixth shape
+  s.params[0] = v3[0];
+  s.params[1] = v3[1];
+  s.params[2] = v3[2];
+  s.params[3] = 0;
+
+  s.color.r = 1;
+  s.color.g = 0;
+  s.color.b = 0;
+  s.color.a = 1;
+
+  pt.x = 0;
+  pt.y = 0;
+  pt.z = 0;
+  pc.push_back (pt);
+
+  pt.x = 0.0;
+  pt.y = 0.0;
+  pt.z = 1.0;
+  pc.push_back (pt);
+
+  pt.x = 1.0;
+  pt.y = 0.0;
+  pt.z = 1.0;
+  pc.push_back (pt);
+
+  Eigen::VectorXf centroid;
+  pcl::computeNDCentroid (pc, centroid);
+  std::cout<<" centroid : "<<centroid<<std::endl;
+
+  pcl::toROSMsg (pc, pc2);
+  s.points.push_back (pc2);
 }
 
 int
 main (int argc, char **argv)
 {
 
-  ros::init (argc, argv, "talker");
+  ros::init (argc, argv, "test_shape_array");
   ros::NodeHandle n;
   ros::Publisher pub = n.advertise<cob_3d_mapping_msgs::ShapeArray> ("shapes_array", 1);
 
@@ -59,265 +382,34 @@ main (int argc, char **argv)
     s.header.stamp = sa.header.stamp;
 
     s.type = cob_3d_mapping_msgs::Shape::PLANE;
-
-    pcl::PointXYZ pt;
-    pcl::PointCloud<pcl::PointXYZ> pc;
-    sensor_msgs::PointCloud2 pc2;
-    //pcl::io::loadPCDFile("")
-/*
-    // Normal estimation*
-      pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
-      pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal>);
-      pcl::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::KdTree<pcl::PointXYZ>());
-      //tree->setInputCloud (pc);
-      //ne.setInputCloud (pc);
-      //ne.setSearchMethod (tree);
-      //ne.setKSearch (20);
-      //ne.compute (*normals);
-      // normals should not contain the point normals + surface curvatures
-*/
-
-    //first shape
-    s.params[0] = 0;
-    s.params[1] = 0;
-    s.params[2] = 1;
-    s.params[3] = 0;
-
-    s.centroid.x = 0.5;
-    s.centroid.y = 0.5;
-    s.centroid.z = 1.0;
-
-    s.color.r = 1;
-    s.color.g = 0;
-    s.color.b = 0;
-    s.color.a = 1;
-
-    pt.x = 0.0;
-    pt.y = 0.0;
-    pt.z = 1.0;
-    pc.push_back (pt);
-
-    pt.x = 1.0;
-    pt.y = 0.0;
-    pt.z = 1.0;
-    pc.push_back (pt);
-
-    pt.x = 1.0;
-    pt.y = 1.0;
-    pt.z = 1.0;
-    pc.push_back (pt);
-
-    pt.x = 0.0;
-    pt.y = 1.0;
-    pt.z = 1.0;
-    pc.push_back (pt);
-
-    pt.x = 0.0;
-    pt.y = 0.0;
-    pt.z = 1.0;
-    pc.push_back (pt);
-
-    /*
-     pt.x = 0.0;
-     pt.y = 0.0;
-     pt.z = 3.0;
-     pc.push_back (pt);
-
-     pt.x = 3.0;
-     pt.y = 3.0;
-     pt.z = 0.0;
-     pc.push_back (pt);
-
-     pt.x = 3.0;
-     pt.y = 0.0;
-     pt.z = 3.0;
-     pc.push_back (pt);
-     */
-    pcl::toROSMsg (pc, pc2);
-    s.points.push_back (pc2);
-
     s.holes.push_back (false);
+
+/*
+    firstShape(s);
     sa.shapes.push_back (s);
+    //std::cout<<" shape_size_1 : "<<s.points.size()<<std::endl;
+    s.points.clear ();
+    //std::cout<<" shape_size_1 clear : "<<s.points.size()<<std::endl;
 
-     //second shape
-     s.params[0] = 0;
-     s.params[1] = 0;
-     s.params[2] = 1;
-     s.params[3] = 0;
+    secondShape(s);
+    sa.shapes.push_back (s);
+    s.points.clear ();
 
-     s.centroid.x = 14 / 3;
-     s.centroid.y = 14 / 3;
-     s.centroid.z = 2;
+    thirdShape(s);
+    sa.shapes.push_back (s);
+    s.points.clear ();
 
-     s.color.r = 0;
-     s.color.g = 1;
-     s.color.b = 0;
-     s.color.a = 1;
-
-     pc.clear ();
-     pc2.data.clear ();
-     s.points.clear ();
-
-     pt.x = 2;
-     pt.y = 6;
-     pt.z = 2;
-     pc.push_back (pt);
-
-     pt.x = 6;
-     pt.y = 2;
-     pt.z = 2;
-     pc.push_back (pt);
-
-     pt.x = 6;
-     pt.y = 6;
-     pt.z = 2;
-     pc.push_back (pt);
-
-    /*
-     pt.x = 2;
-     pt.y = 2;
-     pt.z = 2;
-     pc.push_back (pt);
-     */
-
-     // sensor_msgs::PointCloud2 pc2;
-     pcl::toROSMsg (pc, pc2);
-     s.points.push_back (pc2);
-     s.holes.push_back (false);
-     sa.shapes.push_back (s);
-
-     //third shape
-     s.centroid.x = -16 / 3;
-     s.centroid.y = -11 / 3;
-     s.centroid.z = -2;
-
-     s.params[0] = 0;
-     s.params[1] = 0;
-     s.params[2] = -1;
-     s.params[3] = 0;
-
-     s.color.r = 0;
-     s.color.g = 0;
-     s.color.b = 1;
-     s.color.a = 1;
-
-     pc.clear ();
-     pc2.data.clear ();
-     s.points.clear ();
-
-     pt.x = -5;
-     pt.y = -5;
-     pt.z = -2;
-     pc.push_back (pt);
-
-     pt.x = -5;
-     pt.y = -1;
-     pt.z = -2;
-     pc.push_back (pt);
-
-     pt.x = -6;
-     pt.y = -5;
-     pt.z = -2;
-     pc.push_back (pt);
-
-
-     //sensor_msgs::PointCloud2 pc2;
-     pcl::toROSMsg (pc, pc2);
-     s.points.push_back (pc2);
-
-     s.holes.push_back (false);
-     sa.shapes.push_back (s);
-
-     //fourth shape
-     s.centroid.x = -2 / 3;
-     s.centroid.y = -2 / 3;
-     s.centroid.z = 0;
-
-     s.params[0] = 0;
-     s.params[1] = 0;
-     s.params[2] = 1;
-     s.params[3] = 0;
-
-     s.color.r = 1;
-     s.color.g = 0;
-     s.color.b = 1;
-     s.color.a = 1;
-
-     pc.clear ();
-     pc2.data.clear ();
-     s.points.clear ();
-
-     pt.x = -1;
-     pt.y = 0;
-     pt.z = 0;
-     pc.push_back (pt);
-
-     pt.x = -1;
-     pt.y = -1;
-     pt.z = 0;
-     pc.push_back (pt);
-
-     pt.x = 0;
-     pt.y = -1;
-     pt.z = 0;
-     pc.push_back (pt);
-/*
-     pt.x = 0;
-     pt.y = -2;
-     pt.z = 0;
-     pc.push_back (pt);
-  */
-     //sensor_msgs::PointCloud2 pc2;
-     pcl::toROSMsg (pc, pc2);
-     s.points.push_back (pc2);
-
-     s.holes.push_back (false);
-     sa.shapes.push_back (s);
-
-     //fifth shape
-     s.params[0] = 0;
-     s.params[1] = 0;
-     s.params[2] = -1;
-     s.params[3] = 0;
-
-     s.centroid.x = -2;
-     s.centroid.y = -1;
-     s.centroid.z = -3;
-
-     s.color.r = 1;
-     s.color.g = 1;
-     s.color.b = 0;
-     s.color.a = 1;
-
-     pc.clear ();
-     pc2.data.clear ();
-     s.points.clear ();
-
-     pt.x = 0.0;
-     pt.y = 0.0;
-     pt.z = -3.0;
-     pc.push_back (pt);
-
-     pt.x = -3.0;
-     pt.y = 0.0;
-     pt.z = -3.0;
-     pc.push_back (pt);
-
-     pt.x = -3.0;
-     pt.y = -3.0;
-     pt.z = -3.0;
-     pc.push_back (pt);
-/*
-     pt.x = 0.0;
-     pt.y = -3.0;
-     pt.z = -3.0;
-     pc.push_back (pt);
+    fourthShape(s);
+    sa.shapes.push_back (s);
+    s.points.clear ();
 */
-     pcl::toROSMsg (pc, pc2);
-     s.points.push_back (pc2);
+    fifthShape(s);
+    sa.shapes.push_back (s);
+    s.points.clear ();
 
-     s.holes.push_back (false);
-     sa.shapes.push_back (s);
+    sixthShape(s);
+    sa.shapes.push_back (s);
+    s.points.clear ();
 
     pub.publish (sa);
 
