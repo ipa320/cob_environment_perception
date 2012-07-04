4e469285b5590d5126368e475541683fea8159c8
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2012-07-04 08:23:04 +0200

    segmentation: made it fancy, easier to use

--- cob_3d_segmentation/ros/test/toppm.sh
+++ cob_3d_segmentation/ros/test/toppm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for file in $( find test/labeled/*.pcd )
+for file in $( find `rospack find cob_3d_segmentation`/test/labeled/*.pcd )
 do
 rosrun cob_3d_mapping_tools pcd_to_ppm --in $file --out $file.ppm
 done
