84f5acdc568acfde859ae86a87dd70a6fd4af02d
ipa-goa <goa@ipa.fhg.de>
2012-07-09 14:42:41 +0200

    fixed color bug

--- cob_3d_mapping_rviz_plugins/src/shape_marker.cpp
+++ cob_3d_mapping_rviz_plugins/src/shape_marker.cpp
@@ -235,7 +235,7 @@ namespace rviz
 
 
     polygon_->clear();
-    polygon_->begin(createMaterialIfNotExists(new_message->color.r,new_message->color.b,new_message->color.g,new_message->color.a), Ogre::RenderOperation::OT_TRIANGLE_LIST);
+    polygon_->begin(createMaterialIfNotExists(new_message->color.r,new_message->color.g,new_message->color.b,new_message->color.a), Ogre::RenderOperation::OT_TRIANGLE_LIST);
 
     TPPLPoint p1;
 
