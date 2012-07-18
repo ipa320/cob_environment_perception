87431df9ab5f2cd87b4d13dac3bc5e3845ab9bcf
ipa-goa-sf <fuchs_steffen@gmx.net>
2012-07-18 14:35:00 +0200

    pcd-io bug fix, configure polygon

--- cob_3d_mapping_common/common/src/polygon.cpp
+++ cob_3d_mapping_common/common/src/polygon.cpp
@@ -619,7 +619,7 @@ namespace cob_3d_mapping
       // std::cout<<"dot = "<<fabs(connection.dot(normal))<<std::endl;
       //if(fabs(connection.dot(normal)) < (1- merge_settings_.angle_thresh) && fabs(p_map.d-this->d) < merge_settings_.d_thresh)
       if(fabs(p_map.normal.dot(this->normal)) > (merge_settings_.angle_thresh) &&
-         fabs(connection.dot(this->normal)) < 0.1/* < merge_settings_.d_thresh*/)
+         fabs(connection.dot(this->normal)) < merge_settings_.d_thresh)
       //if((fabs(p_map.normal.dot(normal)) > merge_settings_.angle_thresh && fabs(p_map.d-this->d) < merge_settings_.d_thresh))
 
       {
