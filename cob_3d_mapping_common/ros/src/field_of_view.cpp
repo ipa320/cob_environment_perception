91cdeeda0b2a3ea94149127fe3bcae9f36e02f57
ipa-goa <goa@ipa.fhg.de>
2012-09-27 17:05:54 +0200

    added license header

--- cob_3d_mapping_common/ros/src/field_of_view.cpp
+++ cob_3d_mapping_common/ros/src/field_of_view.cpp
@@ -1,40 +1,45 @@
-/****************************************************************
+/*!
+*****************************************************************
+* \file
 *
-* Copyright (c) 2010
-*
+* \note
+* Copyright (c) 2012 \n
 * Fraunhofer Institute for Manufacturing Engineering
-* and Automation (IPA)
+* and Automation (IPA) \n\n
 *
-* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*****************************************************************
 *
-* Project name: care-o-bot
-* ROS stack name: cob_vision
-* ROS package name: cob_env_model
-* Description:
+* \note
+* Project name: Care-O-bot
+* \note
+* ROS stack name: cob_environment_perception
+* \note
+* ROS package name: cob_3d_mapping_common
 *
-* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-*
+* \author
 * Author: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+* \author
 * Supervised by: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
 *
-* Date of creation: 02/2011
-* ToDo:
-* Publish frustum for visualization
+* \date Date of creation: 02/2011
 *
-* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+* \brief
+* Computes field of view of camera sensors.
+*
+*****************************************************************
 *
 * Redistribution and use in source and binary forms, with or without
 * modification, are permitted provided that the following conditions are met:
 *
-* * Redistributions of source code must retain the above copyright
-* notice, this list of conditions and the following disclaimer.
-* * Redistributions in binary form must reproduce the above copyright
+* - Redistributions of source code must retain the above copyright
+* notice, this list of conditions and the following disclaimer. \n
+* - Redistributions in binary form must reproduce the above copyright
 * notice, this list of conditions and the following disclaimer in the
-* documentation and/or other materials provided with the distribution.
-* * Neither the name of the Fraunhofer Institute for Manufacturing
+* documentation and/or other materials provided with the distribution. \n
+* - Neither the name of the Fraunhofer Institute for Manufacturing
 * Engineering and Automation (IPA) nor the names of its
 * contributors may be used to endorse or promote products derived from
-* this software without specific prior written permission.
+* this software without specific prior written permission. \n
 *
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Lesser General Public License LGPL as
