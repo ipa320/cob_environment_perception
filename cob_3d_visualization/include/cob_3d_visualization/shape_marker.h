a4c7de2437e69bdc9c6ca5e7209a6e2ffba0ebb3
Thomas Zwoelfer <t.zwoelfer@gmx.de>
2012-10-05 14:33:10 +0200

    fixed id and cylinder stuff

--- cob_3d_visualization/include/cob_3d_visualization/shape_marker.h
+++ cob_3d_visualization/include/cob_3d_visualization/shape_marker.h
@@ -53,6 +53,7 @@
 #include <cob_3d_mapping_msgs/ModifyMap.h>
 #include <cob_3d_mapping_common/ros_msg_conversions.h>
 #include "cob_3d_mapping_common/polygon.h"
+#include "cob_3d_mapping_common/cylinder.h"
 //#include <cob_3d_visualization/shape_visualization.h>
 
 //#define PI 3.14159265
