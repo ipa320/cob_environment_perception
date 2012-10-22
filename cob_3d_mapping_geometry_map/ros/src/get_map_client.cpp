4fae0a94b2336b93a901ebce9bae9b38b47c51d1
ipa-goa <goa@ipa.fhg.de>
2012-10-22 14:33:42 +0200

    removed obsolete lines

--- cob_3d_mapping_geometry_map/ros/src/get_map_client.cpp
+++ cob_3d_mapping_geometry_map/ros/src/get_map_client.cpp
@@ -58,30 +58,12 @@
 #include <rosbag/bag.h>
 
 // ROS message includes
-#include <cob_3d_mapping_msgs/GetGeometricMap.h>
+#include <cob_3d_mapping_msgs/GetGeometryMap.h>
 #include <cob_3d_mapping_msgs/ShapeArray.h>
 
 // PCL includes
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-
-#include <iostream>
-#include <fstream>
-
-void writeToFile(cob_3d_mapping_msgs::Shape& s, std::ofstream& fs)
-{
-  //static int ctr=0;
-  //std::stringstream ss;
-  //ss << "/home/goa/pcl_daten/kitchen_kinect/polygons/polygon_" << ctr << ".txt";
-  //std::ofstream myfile;
-  //myfile.open (ss.str().c_str());
-  fs << (int)s.type << "\n";
-  for(unsigned int i=0; i<s.params.size(); i++)
-    fs << s.params[i] << " ";
-
-  //myfile.close();
-
-}
 
 
 int main (int argc, char **argv)
@@ -100,8 +82,8 @@ int main (int argc, char **argv)
   ROS_INFO("Server started, polling map.");
 
   //build message
-  cob_3d_mapping_msgs::GetGeometricMapRequest req;
-  cob_3d_mapping_msgs::GetGeometricMapResponse resp;
+  cob_3d_mapping_msgs::GetGeometryMapRequest req;
+  cob_3d_mapping_msgs::GetGeometryMapResponse resp;
 
   if (ros::service::call("/geometry_map/get_map", req,resp))
   {
@@ -113,35 +95,9 @@ int main (int argc, char **argv)
     return 0;
   }
 
-  // maybe better dump to bag file?
-  /*for(unsigned int i=0; i<resp.shapes.size(); i++)
-  {
-    std::stringstream ss;
-    ss << "shape_" << i << ".txt";
-    std::ofstream fs;
-    fs.open (ss.str().c_str());
-    writeToFile(resp.shapes[i], fs);
-    fs.close();
-    for(unsigned int j=0; j<resp.shapes[i].points.size(); j++)
-    {
-      std::stringstream ss1;
-      ss1 << "shape_" << i << "_" << j << ".pcd";
-      pcl::PointCloud<pcl::PointXYZ> map;
-      pcl::fromROSMsg(resp.shapes[i].points[j], map);
-      pcl::io::savePCDFile(ss1.str(),map,false);
-    }
-  }*/
-  //cob_3d_mapping_msgs::ShapeArray& sa;
-  //cob_3d_mapping_msgs::ShapeArray sa = resp.shapes.shapes;
-  //sa.header.stamp = ros::Time(1.0);
-  //std::cout << sa.header.stamp << std::endl;
-  //sa.header.frame_id = "/map";
-  /*for(unsigned int i=0; i<resp.shapes.size(); i++)
-    sa.shapes.push_back(resp.shapes[i]);*/
-
   rosbag::Bag bag;
   bag.open(argv[1], rosbag::bagmode::Write);
-  bag.write("geometry_map", resp.map.header.stamp, resp.map);
+  bag.write("/geometry_map/map_array", resp.map.header.stamp, resp.map);
 
   bag.close();
 
