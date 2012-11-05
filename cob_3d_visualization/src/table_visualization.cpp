d6e38c9e70d366351a3aea4a3bcd6325173575dc
ipa-goa <goa@ipa.fhg.de>
2012-11-05 14:41:36 +0100

    uses table shape array directly now

--- cob_3d_visualization/src/table_visualization.cpp
+++ cob_3d_visualization/src/table_visualization.cpp
@@ -19,7 +19,7 @@
 void TableVisualization::tableVisualizationCallback (const cob_3d_mapping_msgs::ShapeArrayPtr& ta) {
 
   // Service request and response
-  cob_3d_mapping_msgs::GetObjectsOfClass::Request reqObject;
+  /*cob_3d_mapping_msgs::GetObjectsOfClass::Request reqObject;
   cob_3d_mapping_msgs::GetObjectsOfClass::Response resObject;
 
   cob_3d_mapping_msgs::GetTables::Request reqTable ;
@@ -32,15 +32,15 @@ void TableVisualization::tableVisualizationCallback (const cob_3d_mapping_msgs::
 
   if (ros::service::call("/table_extraction/get_tables",reqTable,resTable)){
     ROS_INFO("Service called...");
-  }
+  }*/
 
   //  std::cout << "response size: "<< "\t" << res.tables.size() << "\n" ;
-  if (!resTable.tables.empty()){
+  /*if (!resTable.tables.empty()){
     for (unsigned int i=0;i<resTable.tables.size();i++){
       ROS_WARN("table id : %d", i) ;//res.objects.shapes[i].id) ;
 //      ROS_WARN("table x_min is: %f , table y_min is: %f", resTable.tables[i].table.x_min , resTable.tables[i].table.y_min);
     }
-  }
+  }*/
   //    tables->shapes[i] = res.objects.shapes[i] ;
   //      boost::shared_ptr<TableMarker> tm(new TableMarker(table_im_server_,res.objects.shapes[i],i)) ;//,ctr_));
   //      v_tm_.push_back(tm) ;
@@ -49,12 +49,12 @@ void TableVisualization::tableVisualizationCallback (const cob_3d_mapping_msgs::
 
 
   //  std::cout << "response size: "<< "\t" << res.objects.shapes.size() << "\n" ;
-  if (!resObject.objects.shapes.empty()){
-    for (unsigned int i=0;i<resObject.objects.shapes.size();i++){
+  if (!ta->shapes.empty()){
+    for (unsigned int i=0;i<ta->shapes.size();i++){
       ROS_WARN("table id : %d", i) ;//res.objects.shapes[i].id) ;
 
       //    tables->shapes[i] = res.objects.shapes[i] ;
-      boost::shared_ptr<TableMarker> tm(new TableMarker(table_im_server_,resObject.objects.shapes[i],i,resTable.tables[i].table)) ;//,ctr_));
+      boost::shared_ptr<TableMarker> tm(new TableMarker(table_im_server_,ta->shapes[i],i/*,resTable.tables[i].table*/)) ;//,ctr_));
       v_tm_.push_back(tm) ;
       //    ctr_ ++ ;
     }
