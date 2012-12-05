e0b829d67abdc9a922b032438be1a46ff1b41fe1
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2012-12-05 09:40:06 +0100

    modified marker

--- cob_3d_mapping_slam/common/include/cob_3d_mapping_slam/marker/marker_container.h
+++ cob_3d_mapping_slam/common/include/cob_3d_mapping_slam/marker/marker_container.h
@@ -8,13 +8,14 @@
 #ifndef MARKER_CONTAINER_H_
 #define MARKER_CONTAINER_H_
 
+#include <rosbag/bag.h>
 #include "marker_list.h"
 
 namespace cob_3d_marker {
 
   class MarkerCreator {
   public:
-    virtual void marker(cob_3d_marker::MarkerContainer &mc)=0;
+    virtual void marker(cob_3d_marker::MarkerContainer &mc) const=0;
   };
 
   class MarkerWrite {
@@ -35,13 +36,16 @@ namespace cob_3d_marker {
   class MarkerBagfile : public MarkerWrite {
     rosbag::Bag *bag_;
     std::string topic_;
+    ros::Time time_;
   public:
-    MarkerBagfile(rosbag::Bag *bag, const std::string topic="/markers"):bag_(bag),topic_(topic) {}
+    MarkerBagfile(rosbag::Bag *bag, const std::string topic="/markers", const ros::Time &time = ros::Time::now()):bag_(bag),topic_(topic), time_(time) {}
 
     virtual void write(const visualization_msgs::Marker &marker) {
-      bag_->write(topic_, ros::Time::now(), marker);
+      bag_->write(topic_, time_, marker);
     }
   };
+
+  class MarkerClean {};
 
   class MarkerContainer {
     typedef std::map<int, boost::shared_ptr<MarkerList> > MT;
@@ -50,7 +54,12 @@ namespace cob_3d_marker {
 
     void add(MarkerList *m) {markers_[m->getId()].reset(m);}
 
-    boost::shared_ptr<MarkerList> get(const int id) {return markers_[id];}
+    boost::shared_ptr<MarkerList> get(const int id) {
+      MT::iterator it = markers_.find(id);
+      if(it==markers_.end())
+        return boost::shared_ptr<MarkerList>();
+      return it->second;
+    }
 
     void write(MarkerWrite *mw) {
       for(MT::const_iterator it=markers_.begin(); it!=markers_.end(); it++)
@@ -63,10 +72,16 @@ namespace cob_3d_marker {
     }
 
     MarkerContainer &operator<<(MarkerList *m) {add(m);return *this;}
-    MarkerContainer &operator<<(MarkerCreator &c) {c.marker(*this);return *this;}
+    MarkerContainer &operator<<(const MarkerCreator &c) {c.marker(*this);return *this;}
+    MarkerContainer &operator<<(const MarkerClean &c) {clean();return *this;}
 
     MarkerContainer &operator>>(MarkerWrite *bag) {write(bag);return *this;}
 
+    void clean() {
+      for(MT::const_iterator it=markers_.begin(); it!=markers_.end(); it++)
+        for(size_t i=0; i<it->second->marker_.size(); i++)
+          it->second->marker_[i].action = visualization_msgs::Marker::DELETE;
+    }
   };
 }
 
