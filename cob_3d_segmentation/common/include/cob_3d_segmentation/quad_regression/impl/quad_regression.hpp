77adeb8a9ec3a0bdc38b54ca5cf3cbf344ce2f19
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2013-01-24 16:50:53 +0100

    outsourced eval. tools

--- cob_3d_segmentation/common/include/cob_3d_segmentation/quad_regression/impl/quad_regression.hpp
+++ cob_3d_segmentation/common/include/cob_3d_segmentation/quad_regression/impl/quad_regression.hpp
@@ -3,6 +3,8 @@
 #include "../sub_structures/labeling.h"
 #include "../sub_structures/debug.h"
 
+#include <cob_3d_segmentation/eval.h>
+
 #include <cob_3d_mapping_common/stop_watch.h>
 
 
@@ -803,63 +805,6 @@ void Segmentation_QuadRegression<Point,PointLabel>::prepare(const pcl::PointClou
 
     return out;
   }
-
-  class RunningStat
-  {
-  public:
-    RunningStat() : m_n(0),m_newS(0) {}
-
-    void Clear()
-    {
-      m_n = 0;
-    }
-
-    void Push(double x)
-    {
-      m_n++;
-
-      // See Knuth TAOCP vol 2, 3rd edition, page 232
-      if (m_n == 1)
-      {
-        m_oldM = m_newM = x;
-        m_oldS = 0.0;
-      }
-      else
-      {
-        m_newM = m_oldM + (x - m_oldM)/m_n;
-        m_newS = m_oldS + (x - m_oldM)*(x - m_newM);
-
-        // set up for next iteration
-        m_oldM = m_newM;
-        m_oldS = m_newS;
-      }
-    }
-
-    int NumDataValues() const
-    {
-      return m_n;
-    }
-
-    double Mean() const
-    {
-      return (m_n > 0) ? m_newM : 0.0;
-    }
-
-    double Variance() const
-    {
-      return ( (m_n > 1) ? m_newS/(m_n - 1) : 0.0 );
-    }
-
-    double StandardDeviation() const
-    {
-      return sqrt( Variance() );
-    }
-
-  private:
-    int m_n;
-    double m_oldM, m_newM, m_oldS, m_newS;
-  };
-
 
   template <typename Point, typename PointLabel>
   void Segmentation_QuadRegression<Point,PointLabel>::compute_accuracy(float &mean, float &var, size_t &used, size_t &mem, size_t &points, float &avg_dist)
