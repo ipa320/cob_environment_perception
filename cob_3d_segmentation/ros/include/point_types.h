090e768dd2d68be13d8495e2d13c6a8a3056e2d1
Your full name <joshua.hampp@ipa.fraunhofer.de>
2013-03-15 06:58:49 +0100

    added point types

---
+++ cob_3d_segmentation/ros/include/point_types.h
@@ -0,0 +1,57 @@
+/*
+ * point_types.h
+ *
+ *  Created on: 14.03.2013
+ *      Author: josh
+ */
+
+#ifndef POINT_TYPES_H_
+#define POINT_TYPES_H_
+
+
+struct PointXYZRGBLabel
+{
+  PCL_ADD_POINT4D; // This adds the members x,y,z which can also be accessed using the point (which is float[4])
+  union
+  {
+    union
+    {
+      struct
+      {
+        uint8_t b;
+        uint8_t g;
+        uint8_t r;
+        uint8_t _unused;
+      };
+      float rgb;
+    };
+    uint32_t rgba;
+  };
+  uint32_t label;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+    PointXYZRGBLabel,
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (float, rgb, rgb)
+    (uint32_t, label, label));
+
+
+struct PointXYZLabel
+{
+  PCL_ADD_POINT4D; // This adds the members x,y,z which can also be accessed using the point (which is float[4])
+  uint32_t label;
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+} EIGEN_ALIGN16;
+
+POINT_CLOUD_REGISTER_POINT_STRUCT(
+    PointXYZLabel,
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (uint32_t, label, label));
+
+#endif /* POINT_TYPES_H_ */
