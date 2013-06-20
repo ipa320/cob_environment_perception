3ec99c9794680e2d11aaf972a3a71f8820535c9d
ipa-goa-sf <fuchs_steffen@gmx.net>
2013-06-20 14:00:57 +0200

    added a comment

--- cob_3d_mapping_common/common/include/cob_3d_mapping_common/sensor_model.h
+++ cob_3d_mapping_common/common/include/cob_3d_mapping_common/sensor_model.h
@@ -433,6 +433,7 @@ namespace cob_3d_mapping
 
     static bool areNeighbors(const Eigen::Vector3f& query, const Eigen::Vector3f&  neighbor, float tolerance = 4.0f)
     {
+      // += max( 0, 4-10*min(|x|,|y|) )
       tolerance += std::max( 0.0, 1.0f * (4.0f - std::min(fabs(query(0)),fabs(query(1))) * 10.0f) );
       float dist_th =  tolerance * 0.003f * query(2) * query(2);
       return (fabs(query(2) - neighbor(2)) < dist_th);
