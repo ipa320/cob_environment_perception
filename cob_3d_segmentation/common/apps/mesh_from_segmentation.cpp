fd340e4b8e3176a327274d6d4b368b26f5742ab3
ipa-goa-sf <fuchs_steffen@gmx.net>
2013-06-29 01:03:58 +0200

    started mesh simplification approach

--- cob_3d_segmentation/common/apps/mesh_from_segmentation.cpp
+++ cob_3d_segmentation/common/apps/mesh_from_segmentation.cpp
@@ -1,3 +1,64 @@
+/*!
+ *****************************************************************
+ * \file
+ *
+ * \note
+ *   Copyright (c) 2012 \n
+ *   Fraunhofer Institute for Manufacturing Engineering
+ *   and Automation (IPA) \n\n
+ *
+ *****************************************************************
+ *
+ * \note
+ *  Project name: care-o-bot
+ * \note
+ *  ROS stack name: cob_environment_perception_intern
+ * \note
+ *  ROS package name: cob_3d_segmentation
+ *
+ * \author
+ *  Author: Steffen Fuchs, email:georg.arbeiter@ipa.fhg.de
+ * \author
+ *  Supervised by: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
+ *
+ * \date Date of creation: 06/2013
+ *
+ * \brief
+ * Description:
+ *
+ * ToDo:
+ *
+ *
+ *****************************************************************
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer. \n
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution. \n
+ *     - Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission. \n
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
 
 #include <boost/program_options.hpp>
 
@@ -6,6 +67,7 @@
 #include <cob_3d_mapping_common/stop_watch.h>
 #include "cob_3d_mapping_filters/downsample_filter.h"
 #include "cob_3d_mapping_features/organized_normal_estimation_omp.h"
+#include "cob_3d_mapping_features/organized_curvature_estimation_omp.h"
 #include "cob_3d_segmentation/impl/fast_segmentation.hpp"
 #include "cob_3d_segmentation/polygon_extraction/polygon_extraction.h"
 
@@ -18,6 +80,7 @@ namespace cob_3d_segmentation
   public:
     typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloud;
     typedef pcl::PointCloud<pcl::Normal> NormalCloud;
+    typedef pcl::PointCloud<pcl::PrincipalCurvature> CurvatureCloud;
     typedef pcl::PointCloud<PointLabel> LabelCloud;
     typedef cob_3d_segmentation::FastSegmentation<pcl::PointXYZRGB, pcl::Normal, PointLabel>::ClusterPtr ClusterPtr;
 
@@ -29,6 +92,7 @@ namespace cob_3d_segmentation
       , down_(new PointCloud)
       , segmented_(new PointCloud)
       , normals_(new NormalCloud)
+      , curvatures_(new CurvatureCloud)
       , labels_(new LabelCloud)
       {
         one_.setOutputLabels(labels_);
@@ -76,6 +140,16 @@ namespace cob_3d_segmentation
       one_.setInputCloud(down_);
       one_.compute(*normals_);
       std::cout << "normals took " << t.precisionStop() << " s." << std::endl;
+
+      t.precisionStart();
+      LabelCloud::Ptr fake_labels(new LabelCloud);
+      oce_.setInputCloud(down_);
+      oce_.setInputNormals(normals_);
+      oce_.setPicelSearchRadius(8,2,2);
+      oce_.setSkipDistantPointThreshold(8);
+      oce_.setOutputLabels(l);
+      oce_.compute(*curvatures_);
+      std::cout << "curvatures took " << t.precisionStop() << " s." << std::endl;
 
       t.precisionStart();
       seg_.setInputCloud(down_);
@@ -181,6 +255,7 @@ namespace cob_3d_segmentation
     }
 
     cob_3d_mapping_features::OrganizedNormalEstimationOMP<pcl::PointXYZRGB, pcl::Normal, PointLabel> one_;
+    cob_3d_mapping_features::OrganizedCurvatureEstimationOMP<pcl::PointXYZRGB, pcl::Normal, PointLabel, pcl::PrincipalCurvature> oce_;
     cob_3d_segmentation::FastSegmentation<pcl::PointXYZRGB, pcl::Normal, PointLabel,
                                           cob_3d_segmentation::Options::ComputeGraph> seg_;
     //cob_3d_segmentation::FastSegmentation<pcl::PointXYZRGB, pcl::Normal, PointLabel> seg_;
@@ -189,6 +264,7 @@ namespace cob_3d_segmentation
     PointCloud::Ptr down_;
     PointCloud::Ptr segmented_;
     NormalCloud::Ptr normals_;
+    Curvature::Ptr curvatures_;
     LabelCloud::Ptr labels_;
     LabelCloud::Ptr borders_;
   };
