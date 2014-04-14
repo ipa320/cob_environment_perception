66b9b11ab05e697be50232743c904447bb8ebc09
ipa-goa <goa@ipa.fhg.de>
2014-04-14 16:02:38 +0200

    improved profiling, added OpenMP support

--- cob_3d_features/common/test/profile_ne.cpp
+++ cob_3d_features/common/test/profile_ne.cpp
@@ -10,18 +10,18 @@
  *****************************************************************
  *
  * \note
- *  Project name: TODO FILL IN PROJECT NAME HERE
+ *  Project name: care-o-bot
  * \note
- *  ROS stack name: TODO FILL IN STACK NAME HERE
+ *  ROS stack name: cob_environment_perception
  * \note
- *  ROS package name: TODO FILL IN PACKAGE NAME HERE
+ *  ROS package name: cob_3d_features
  *
  * \author
- *  Author: TODO FILL IN AUTHOR NAME HERE
+ *  Author: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
  * \author
- *  Supervised by: TODO FILL IN CO-AUTHOR NAME(S) HERE
+ *  Supervised by: Georg Arbeiter, email:georg.arbeiter@ipa.fhg.de
  *
- * \date Date of creation: TODO FILL IN DATE HERE
+ * \date Date of creation: 04/2014
  *
  * \brief
  *
@@ -56,6 +56,7 @@
  * If not, see <http://www.gnu.org/licenses/>.
  *
  ****************************************************************/
+
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/features/integral_image_normal.h>
@@ -73,7 +74,8 @@
 
 using namespace pcl;
 
-void determinePlaneNormal(PointCloud<PointXYZ>::Ptr& p, Eigen::Vector3f& normal)
+void
+determinePlaneNormal (PointCloud<PointXYZ>::Ptr& p, Eigen::Vector3f& normal)
 {
   SACSegmentation<PointXYZ> seg;
   seg.setOptimizeCoefficients (true);
@@ -85,100 +87,133 @@ void determinePlaneNormal(PointCloud<PointXYZ>::Ptr& p, Eigen::Vector3f& normal)
   ModelCoefficients coefficients_plane;
   pcl::PointIndices inliers_plane;
   seg.segment (inliers_plane, coefficients_plane);
-  normal(0) = coefficients_plane.values[0];
-  normal(1) = coefficients_plane.values[1];
-  normal(2) = coefficients_plane.values[2];
+  normal (0) = coefficients_plane.values[0];
+  normal (1) = coefficients_plane.values[1];
+  normal (2) = coefficients_plane.values[2];
 }
 
-int main(int argc, char** argv)
+int
+main (int argc, char** argv)
 {
-  PointCloud<PointXYZ>::Ptr p(new PointCloud<PointXYZ>);
-  PointCloud<Normal>::Ptr n(new PointCloud<Normal>);
-  PointCloud<InterestPoint>::Ptr ip(new PointCloud<InterestPoint>);
+  if (argc < 2)
+  {
+    std::cerr << "Error: Please specify a PCD file (rosrun cob_3d_features profile_ne test.pcd)." << std::endl;
+    return -1;
+  }
+  PointCloud<PointXYZ>::Ptr p (new PointCloud<PointXYZ>);
+  PointCloud<Normal>::Ptr n (new PointCloud<Normal>);
+  PointCloud<InterestPoint>::Ptr ip (new PointCloud<InterestPoint>);
   pcl::PointCloud<pcl::PointNormal> p_n;
   PrecisionStopWatch t;
   std::string file_ = argv[1];
   PCDReader r;
-  if (r.read(file_, *p) == -1) return(0);
+  if (r.read (file_, *p) == -1)
+    return -1;
 
   Eigen::Vector3f normal;
-  determinePlaneNormal(p, normal);
-  std::cout << normal << std::endl;
+  determinePlaneNormal (p, normal);
+  //std::cout << normal << std::endl;
 
-  cob_3d_features::OrganizedNormalEstimationOMP<PointXYZ,Normal,PointLabel> ne;
-  ne.setPixelSearchRadius(8,2,2);
-  ne.setInputCloud(p);
-  PointCloud<PointLabel>::Ptr labels(new PointCloud<PointLabel>);
-  ne.setOutputLabels(labels);
-  t.precisionStart();
-  ne.compute(*n);
-  std::cout << t.precisionStop() << "s\t for organized normal estimation" << std::endl;
+  cob_3d_features::OrganizedNormalEstimation<PointXYZ, Normal, PointLabel> ne;
+  ne.setPixelSearchRadius (8, 2, 2);
+  //ne.setSkipDistantPointThreshold(8);
+  ne.setInputCloud (p);
+  PointCloud<PointLabel>::Ptr labels (new PointCloud<PointLabel>);
+  ne.setOutputLabels (labels);
+  t.precisionStart ();
+  ne.compute (*n);
+  std::cout << t.precisionStop () << "s\t for organized normal estimation" << std::endl;
+
+  cob_3d_features::OrganizedNormalEstimationOMP<PointXYZ, Normal, PointLabel> ne_omp;
+  ne_omp.setPixelSearchRadius (8, 2, 2);
+  //ne.setSkipDistantPointThreshold(8);
+  ne_omp.setInputCloud (p);
+  //PointCloud<PointLabel>::Ptr labels(new PointCloud<PointLabel>);
+  ne_omp.setOutputLabels (labels);
+  t.precisionStart ();
+  ne_omp.compute (*n);
+  std::cout << t.precisionStop () << "s\t for organized normal estimation (OMP)" << std::endl;
   concatenateFields (*p, *n, p_n);
-  io::savePCDFileASCII ("/home/goa/pcl_daten/normal_estimation/normals_organized.pcd", p_n);
+  io::savePCDFileASCII (".ros/normals_organized.pcd", p_n);
 
-  double good_thr=0.97;
-  unsigned int ctr=0, nan_ctr=0;
-  double d_sum=0;
-  for(unsigned int i=0; i<p->size(); i++)
+  double good_thr = 0.97;
+  unsigned int ctr = 0, nan_ctr = 0;
+  double d_sum = 0;
+  for (unsigned int i = 0; i < p->size (); i++)
   {
-    if ( pcl_isnan(n->points[i].normal[0]) ) {nan_ctr++;continue;}
-    double d = normal.dot(n->points[i].getNormalVector3fMap());
-    d_sum += fabs(1-fabs(d));
-    if(fabs(d)>good_thr) ctr++;
+    if (pcl_isnan(n->points[i].normal[0]))
+    {
+      nan_ctr++;
+      continue;
+    }
+    double d = normal.dot (n->points[i].getNormalVector3fMap ());
+    d_sum += fabs (1 - fabs (d));
+    if (fabs (d) > good_thr)
+      ctr++;
   }
-  std::cout << "Average error: " << d_sum/p->size() << std::endl;
-  std::cout << "Ratio of good normals: " << (double)ctr/p->size() << std::endl;
+  std::cout << "Average error: " << d_sum / p->size () << std::endl;
+  std::cout << "Ratio of good normals: " << (double)ctr / p->size () << std::endl;
   std::cout << "Invalid normals: " << nan_ctr << std::endl;
 
-  IntegralImageNormalEstimation<PointXYZ,Normal> ne2;
+  IntegralImageNormalEstimation<PointXYZ, Normal> ne2;
   ne2.setNormalEstimationMethod (ne2.COVARIANCE_MATRIX);
-  ne2.setMaxDepthChangeFactor(0.02f);
-  ne2.setNormalSmoothingSize(10.0f);
-  ne2.setDepthDependentSmoothing(true);
-  ne2.setInputCloud(p);
-  t.precisionStart();
-  ne2.compute(*n);
-  std::cout << t.precisionStop() << "s\t for integral image normal estimation" << std::endl;
+  ne2.setMaxDepthChangeFactor (0.02f);
+  ne2.setNormalSmoothingSize (10.0f);
+  ne2.setDepthDependentSmoothing (true);
+  ne2.setInputCloud (p);
+  t.precisionStart ();
+  ne2.compute (*n);
+  std::cout << t.precisionStop () << "s\t for integral image normal estimation" << std::endl;
   concatenateFields (*p, *n, p_n);
-  io::savePCDFileASCII ("/home/goa/pcl_daten/normal_estimation/normals_integral.pcd", p_n);
+  io::savePCDFileASCII (".ros/normals_integral.pcd", p_n);
 
-  ctr=0;
-  nan_ctr=0;
-  d_sum=0;
-  for(unsigned int i=0; i<p->size(); i++)
+  ctr = 0;
+  nan_ctr = 0;
+  d_sum = 0;
+  for (unsigned int i = 0; i < p->size (); i++)
   {
-    if ( pcl_isnan(n->points[i].normal[0]) ) {nan_ctr++;continue;}
-    double d = normal.dot(n->points[i].getNormalVector3fMap());
-    d_sum += fabs(1-fabs(d));
-    if(fabs(d)>good_thr) ctr++;
+    if (pcl_isnan(n->points[i].normal[0]))
+    {
+      nan_ctr++;
+      continue;
+    }
+    double d = normal.dot (n->points[i].getNormalVector3fMap ());
+    d_sum += fabs (1 - fabs (d));
+    if (fabs (d) > good_thr)
+      ctr++;
   }
-  std::cout << "Average error: " << d_sum/p->size() << std::endl;
-  std::cout << "Ratio of good normals: " << (double)ctr/p->size() << std::endl;
+  std::cout << "Average error: " << d_sum / p->size () << std::endl;
+  std::cout << "Ratio of good normals: " << (double)ctr / p->size () << std::endl;
   std::cout << "Invalid normals: " << nan_ctr << std::endl;
 
   NormalEstimationOMP<PointXYZ, Normal> ne3;
-  ne3.setInputCloud(p);
-  ne3.setNumberOfThreads(4);
-  ne3.setKSearch(256);
+  ne3.setInputCloud (p);
+  ne3.setNumberOfThreads (4);
+  ne3.setKSearch (256);
   //ne3.setRadiusSearch(0.01);
-  t.precisionStart();
-  ne3.compute(*n);
-  std::cout << t.precisionStop() << "s\t for vanilla normal estimation" << std::endl;
+  t.precisionStart ();
+  ne3.compute (*n);
+  std::cout << t.precisionStop () << "s\t for vanilla normal estimation" << std::endl;
   concatenateFields (*p, *n, p_n);
-  io::savePCDFileASCII ("/home/goa/pcl_daten/normal_estimation/normals_vanilla.pcd", p_n);
+  io::savePCDFileASCII (".ros/normals_vanilla.pcd", p_n);
 
-  ctr=0;
-  nan_ctr=0;
-  d_sum=0;
-  for(unsigned int i=0; i<p->size(); i++)
+  ctr = 0;
+  nan_ctr = 0;
+  d_sum = 0;
+  for (unsigned int i = 0; i < p->size (); i++)
   {
-    if ( pcl_isnan(n->points[i].normal[0]) ) {nan_ctr++;continue;}
-    double d = normal.dot(n->points[i].getNormalVector3fMap());
-    d_sum += fabs(1-fabs(d));
-    if(fabs(d)>good_thr) ctr++;
+    if (pcl_isnan(n->points[i].normal[0]))
+    {
+      nan_ctr++;
+      continue;
+    }
+    double d = normal.dot (n->points[i].getNormalVector3fMap ());
+    d_sum += fabs (1 - fabs (d));
+    if (fabs (d) > good_thr)
+      ctr++;
   }
-  std::cout << "Average error: " << d_sum/p->size() << std::endl;
-  std::cout << "Ratio of good normals: " << (double)ctr/p->size() << std::endl;
+  std::cout << "Average error: " << d_sum / p->size () << std::endl;
+  std::cout << "Ratio of good normals: " << (double)ctr / p->size () << std::endl;
   std::cout << "Invalid normals: " << nan_ctr << std::endl;
 
   return 0;
