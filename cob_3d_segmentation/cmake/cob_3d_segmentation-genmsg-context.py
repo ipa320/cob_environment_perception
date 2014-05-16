9936b3fd2720b7e305ec5071fe48487170bebded
Your full name <joshua.hampp@ipa.fraunhofer.de>
2014-05-16 09:23:33 +0200

    added missing cmake files

---
+++ cob_3d_segmentation/cmake/cob_3d_segmentation-genmsg-context.py
@@ -0,0 +1,10 @@
+# generated from genmsg/cmake/pkg-genmsg.context.in
+
+messages_str = "/home/josh/workspace/cob_environment_perception/cob_3d_segmentation/devel/share/cob_3d_segmentation/msg/ObjectWatchAction.msg;/home/josh/workspace/cob_environment_perception/cob_3d_segmentation/devel/share/cob_3d_segmentation/msg/ObjectWatchActionGoal.msg;/home/josh/workspace/cob_environment_perception/cob_3d_segmentation/devel/share/cob_3d_segmentation/msg/ObjectWatchActionResult.msg;/home/josh/workspace/cob_environment_perception/cob_3d_segmentation/devel/share/cob_3d_segmentation/msg/ObjectWatchActionFeedback.msg;/home/josh/workspace/cob_environment_perception/cob_3d_segmentation/devel/share/cob_3d_segmentation/msg/ObjectWatchGoal.msg;/home/josh/workspace/cob_environment_perception/cob_3d_segmentation/devel/share/cob_3d_segmentation/msg/ObjectWatchResult.msg;/home/josh/workspace/cob_environment_perception/cob_3d_segmentation/devel/share/cob_3d_segmentation/msg/ObjectWatchFeedback.msg"
+services_str = ""
+pkg_name = "cob_3d_segmentation"
+dependencies_str = "actionlib_msgs;geometry_msgs"
+langs = "gencpp;genlisp;genpy"
+dep_include_paths_str = "cob_3d_segmentation;/home/josh/workspace/cob_environment_perception/cob_3d_segmentation/devel/share/cob_3d_segmentation/msg;actionlib_msgs;/opt/ros/groovy/share/actionlib_msgs/cmake/../msg;geometry_msgs;/opt/ros/groovy/share/geometry_msgs/cmake/../msg;std_msgs;/opt/ros/groovy/share/std_msgs/cmake/../msg"
+PYTHON_EXECUTABLE = "/usr/bin/python"
+package_has_static_sources = '' == 'TRUE'
