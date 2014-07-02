56db82909c4cd0fb87d491e9703ee9924f018f85
Your full name <joshua.hampp@ipa.fraunhofer.de>
2014-07-02 07:47:07 +0200

    update to eval

--- cob_3d_features/common/test/test_invariant_feature_eval.hpp
+++ cob_3d_features/common/test/test_invariant_feature_eval.hpp
@@ -48,10 +48,11 @@ void _compute<pcl::SHOT352>(pcl::PointCloud<pcl::SHOT352>::Ptr &features, const 
   pcl::SHOTEstimationOMP<pcl::PointNormal, pcl::PointNormal, pcl::SHOT352> descr_est;
   descr_est.setRadiusSearch (radius);
 
-  pcl::PointCloud<pcl::PointNormal>::Ptr keypoints(new pcl::PointCloud<pcl::PointNormal>);
-  pcl::copyPointCloud (*p_n2, *indicies, *keypoints);
+  //pcl::PointCloud<pcl::PointNormal>::Ptr keypoints(new pcl::PointCloud<pcl::PointNormal>);
+  //pcl::copyPointCloud (*p_n2, *indicies, *keypoints);
   
-  descr_est.setInputCloud (keypoints);
+  descr_est.setIndices(indicies);
+  descr_est.setInputCloud (p_n2);
   descr_est.setInputNormals (p_n2);
   descr_est.setSearchSurface (p_n2);
   descr_est.compute (*features);
@@ -61,6 +62,7 @@ template<>
 void _compute<pcl::VFHSignature308>(pcl::PointCloud<pcl::VFHSignature308>::Ptr &features, const double radius, pcl::IndicesPtr &indicies, pcl::PointCloud<pcl::PointNormal>::Ptr &p_n2) {
   pcl::CVFHEstimation<pcl::PointNormal, pcl::PointNormal, pcl::VFHSignature308> descr_est;
   descr_est.setRadiusSearch (radius); //not used...
+  descr_est.setKSearch(0);
 
   pcl::search::KdTree<pcl::PointNormal>::Ptr tree (new pcl::search::KdTree<pcl::PointNormal> ());
   descr_est.setSearchMethod (tree);
@@ -76,22 +78,25 @@ template<>
 void _compute<pcl::ESFSignature640>(pcl::PointCloud<pcl::ESFSignature640>::Ptr &features, const double radius, pcl::IndicesPtr &indicies, pcl::PointCloud<pcl::PointNormal>::Ptr &p_n2) {
   pcl::ESFEstimation<pcl::PointNormal, pcl::ESFSignature640> descr_est;
   descr_est.setRadiusSearch (radius);
+  descr_est.setKSearch(0);
   
   descr_est.setInputCloud (p_n2);
   
   pcl::search::KdTree<pcl::PointNormal>::Ptr tree (new pcl::search::KdTree<pcl::PointNormal> ());
   descr_est.setSearchMethod (tree);
-  descr_est.setIndices(indicies);
+  descr_est.setSearchSurface (p_n2);
+  //descr_est.setIndices(indicies);
   
   descr_est.compute (*features);
 }
 
-template<class Feature>
-void computeFeatures(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, const double radius, typename pcl::PointCloud<Feature>::Ptr &features, const std::vector<Eigen::Vector3d> &keypoints) {
+double computeNormals(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, const std::vector<Eigen::Vector3d> &keypoints,
+	pcl::IndicesPtr &indicies, pcl::PointCloud<pcl::PointNormal>::Ptr &p_n2) {
 	if(!cloud) {
 		std::cout<<"no cloud given !!"<<std::endl;
-		return;
+		return -1;
 	}
+	
   pcl::PointCloud<pcl::Normal>::Ptr normals (new pcl::PointCloud<pcl::Normal> ());
   
   PrecisionStopWatch sw;
@@ -105,42 +110,48 @@ void computeFeatures(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, const doubl
     //one.setSkipDistantPointThreshold(12);
     one.compute(*normals);
     
-  std::cout<<"normal estimation "<<sw.precisionStop()<<"s"<<std::endl;
+   const double r = sw.precisionStop();
+    
+  std::cout<<"normal estimation "<<r<<"s"<<std::endl;
 		
   pcl::PointCloud<pcl::PointNormal> p_n;
-  pcl::PointCloud<pcl::PointNormal>::Ptr p_n2(new pcl::PointCloud<pcl::PointNormal>);
+  p_n2.reset(new pcl::PointCloud<pcl::PointNormal>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloud2(new pcl::PointCloud<pcl::PointXYZ>);
   concatenateFields (*cloud, *normals, p_n);
   {
+	  const float leafSize = 0.00003f;
 	std::vector<int> indicies;
 	pcl::removeNaNFromPointCloud(p_n, *p_n2, indicies);
 	
+	std::cout<<"size "<<p_n2->size()<<" -> ";
 	pcl::VoxelGrid<pcl::PointNormal> sor;
 	sor.setInputCloud (p_n2);
-	sor.setLeafSize (0.03f, 0.03f, 0.03f);
+	sor.setLeafSize (leafSize, leafSize, leafSize);
 	sor.filter (*p_n2);
+	std::cout<<p_n2->size()<<std::endl;
 	
 	indicies.clear();
 	pcl::removeNaNFromPointCloud(*cloud, *cloud2, indicies);
 	
 	pcl::VoxelGrid<pcl::PointXYZ> sor2;
 	sor2.setInputCloud (cloud2);
-	sor2.setLeafSize (0.03f, 0.03f, 0.03f);
+	sor2.setLeafSize (leafSize, leafSize, leafSize);
 	sor2.filter (*cloud2);
 	
 	assert(cloud2->size()==p_n2->size());
+	
+	for(size_t i=0; i<p_n2->size(); i++) {
+		if( (*p_n2)[i].normal_x!=(*p_n2)[i].normal_x ) {
+			p_n2->erase(p_n2->begin()+i);
+			cloud2->erase(cloud2->begin()+i);
+			--i;
+		}
+	}
   }
-  
-  /*for(size_t i=0; i<cloud->size(); i++)
-	if((*cloud)[i].x!=(*cloud)[i].x) {
-		(*cloud)[i].x=(*cloud)[i].y=(*cloud)[i].z=-1000;
-	}*/
-
-  sw.precisionStart();
 
   pcl::search::KdTree<pcl::PointXYZ>::Ptr tree2 (new pcl::search::KdTree<pcl::PointXYZ> ());
   tree2->setInputCloud (cloud2);
-  pcl::IndicesPtr indicies(new std::vector<int>);
+  indicies.reset(new std::vector<int>);
   for(size_t i=0; i<keypoints.size(); i++) {
 	  pcl::PointXYZ pt;
 	  pt.x = keypoints[i](0);
@@ -153,13 +164,65 @@ void computeFeatures(pcl::PointCloud<pcl::PointXYZ>::ConstPtr cloud, const doubl
 	  
 	  indicies->push_back(k_indices[0]);
   }
-  std::cout<<"indicies "<<indicies->size()<<std::endl;
-	
+  
+  return r;
+}
+
+template<class Feature>
+double computeFeatures(pcl::IndicesPtr &indicies, pcl::PointCloud<pcl::PointNormal>::Ptr &p_n2, const double radius, typename pcl::PointCloud<Feature>::Ptr &features) {
+	if(!p_n2) {
+		std::cout<<"no cloud given !!"<<std::endl;
+		return -1;
+	}
+
+  PrecisionStopWatch sw;
+  sw.precisionStart();	
 
   // Output datasets
   features.reset(new pcl::PointCloud<Feature> ());
-
   _compute<Feature>(features, radius, indicies, p_n2);
   
-  std::cout<<"feature estimation "<<sw.precisionStop()<<"s"<<std::endl;
+  const double r = sw.precisionStop();
+  
+  std::cout<<"feature estimation "<<r<<"s"<<std::endl;
+  
+  return r;
+}
+
+
+template<class Feature>
+void serialize_feature(const Feature &ft, std::vector<float> &res) {
+	BOOST_STATIC_ASSERT(sizeof(Feature)==-1);
+}
+
+template<>
+void serialize_feature<pcl::FPFHSignature33>(const pcl::FPFHSignature33 &ft, std::vector<float> &res) {
+	const int SIZE=33;
+	res.resize(SIZE);
+	for(int i=0; i<SIZE; i++)
+		res[i] = ft.histogram[i];
+}
+
+template<>
+void serialize_feature<pcl::ESFSignature640>(const pcl::ESFSignature640 &ft, std::vector<float> &res) {
+	const int SIZE=640;
+	res.resize(SIZE);
+	for(int i=0; i<SIZE; i++)
+		res[i] = ft.histogram[i];
+}
+
+template<>
+void serialize_feature<pcl::VFHSignature308>(const pcl::VFHSignature308 &ft, std::vector<float> &res) {
+	const int SIZE=308;
+	res.resize(SIZE);
+	for(int i=0; i<SIZE; i++)
+		res[i] = ft.histogram[i];
+}
+
+template<>
+void serialize_feature<pcl::SHOT352>(const pcl::SHOT352 &ft, std::vector<float> &res) {
+	const int SIZE=352;
+	res.resize(SIZE);
+	for(int i=0; i<SIZE; i++)
+		res[i] = ft.descriptor[i];
 }
