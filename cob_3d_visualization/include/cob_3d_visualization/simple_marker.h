b3574b46c420a506682167ec541bdd6d2010763d
Your full name <joshua.hampp@ipa.fraunhofer.de>
2014-09-03 08:54:55 +0200

    added comments to marker vis

--- cob_3d_visualization/include/cob_3d_visualization/simple_marker.h
+++ cob_3d_visualization/include/cob_3d_visualization/simple_marker.h
@@ -1,3 +1,60 @@
+/*!
+ *****************************************************************
+ * \file
+ *
+ * \note
+ *   Copyright (c) 2012 \n
+ *   Fraunhofer Institute for Manufacturing Engineering
+ *   and Automation (IPA) \n\n
+ *
+ *****************************************************************
+ *
+ * \note
+ *  Project name: care-o-bot
+ * \note
+ *  ROS stack name: cob_environment_perception
+ * \note
+ *  ROS package name: cob_3d_visualization
+ *
+ * \author
+ *  Author: Joshua Hampp, joshua.hampp@ipa.fraunhofer.de
+ *
+ * \date Date of creation: 09/2014
+ *
+ * \brief
+ *
+ *
+ *****************************************************************
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     - Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer. \n
+ *     - Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution. \n
+ *     - Neither the name of the Fraunhofer Institute for Manufacturing
+ *       Engineering and Automation (IPA) nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission. \n
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License LGPL as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License LGPL for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License LGPL along with this program.
+ * If not, see <http://www.gnu.org/licenses/>.
+ *
+ ****************************************************************/
+
 #pragma once
 
 #include <ros/ros.h>
@@ -8,6 +65,26 @@
 /* 
  * EXAMPLE USAGE:
  * 
+ * once at startup for init.:
+ * cob_3d_visualization::RvizMarkerManager::get().createTopic("/marker").setFrameId("/map").clearOld();
+ * 
+ * 1. creates a MarkerArray topic on /marker with the default frame id /map
+ * 2. clears old markers
+ * 
+ * 
+ * to visualize markers:
+ * cob_3d_visualization::RvizMarkerManager::get().clear();
+ * {
+ * 	cob_3d_visualization::RvizMarker scene;
+ * 	scene.sphere(center, 0.05);
+ * 	scene.color(0.1,1.,0.1);
+ * }
+ * cob_3d_visualization::RvizMarkerManager::get().publish();
+ * 
+ * 1. clears markers
+ * 2. creates a sphere marker with radius 0.05 metres at point "center"
+ * 3. sets color
+ * 4. publish all new created markers
  */
 
 namespace cob_3d_visualization {
