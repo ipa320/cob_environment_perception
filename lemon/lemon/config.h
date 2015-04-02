781f76184ab33fa4ac3ebce83996d1eba03ef9d0
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2015-04-02 12:33:18 +0200

    added lemon

---
+++ lemon/lemon/config.h
@@ -0,0 +1,22 @@
+#define LEMON_VERSION "trunk+638-5de6a70446f6"
+#define LEMON_HAVE_LONG_LONG 1
+
+#define LEMON_HAVE_LP 1
+#define LEMON_HAVE_MIP 1
+#define LEMON_HAVE_GLPK 1
+/* #undef LEMON_HAVE_CPLEX */
+#define LEMON_HAVE_SOPLEX 1
+/* #undef LEMON_HAVE_CLP */
+/* #undef LEMON_HAVE_CBC */
+
+#define _LEMON_CPLEX 1
+#define _LEMON_CLP 2
+#define _LEMON_GLPK 3
+#define _LEMON_SOPLEX 4
+#define _LEMON_CBC 5
+
+#define LEMON_DEFAULT_LP _LEMON_GLPK
+#define LEMON_DEFAULT_MIP _LEMON_GLPK
+
+#define LEMON_USE_PTHREAD 1
+/* #undef LEMON_USE_WIN32_THREADS */
