590ea0fd47a8483348788601c17175e7ca5e8934
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2015-04-14 13:38:52 +0200

    updated proximity

--- cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/types/transformation.h
+++ cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/types/transformation.h
@@ -67,6 +67,25 @@ namespace cob_3d_experience_mapping {
 			r(1) = std::sqrt(r(1));
 			return r;
 		}
+
+		TDist energy_distance(const Transformation &tmp, const TDist &thr) const {
+			TDist r = TDist::Zero();
+			TDist l = TDist::Zero();
+			
+			for(int i=0; i<NUM_TRANS; i++) {
+				r(0) += std::pow(tmp(i), 2);
+				l(0) += std::pow(link_(i), 2);
+			}
+			for(int i=NUM_TRANS; i<NUM_TRANS+NUM_ROT; i++) {
+				r(1) += std::pow(tmp(i), 2);
+				l(1) += std::pow(link_(i), 2);
+			}
+				
+			r(0) = std::sqrt(r(0))/std::max(std::sqrt(l(0), thr(0));
+			r(1) = std::sqrt(r(1))/std::max(std::sqrt(l(1), thr(1));
+			
+			return r.norm();
+		}
 		
 		void integrate(const Transformation &movement) {
 			BOOST_STATIC_ASSERT(NUM_TRANS==2);	//TODO: at the moment only implement for 2d case
@@ -81,21 +100,13 @@ namespace cob_3d_experience_mapping {
 		}
 
 		//TODO:
-		TType proximity_neg(const TDist &thr) const {
-			TDist d = distance(Transformation());
-			helper_hystersis<0>(d,thr);
-			helper_hystersis<1>(d,thr);
-			return -(1-d(0))*(1-d(1));
+		TType proximity_neg(const TDist &thr, const TType &k) const {
+			return -1+factor(energy_distance(link_, thr));
 		}
 
 		//TODO:
-		TType proximity_pos(const Transformation &o, const TDist &thr) const {
-			TDist d = distance(o);
-			printf("proximity_pos A %f %f\n", d(0),d(1));
-			helper_hystersis<0>(d,thr);
-			helper_hystersis<1>(d,thr);
-			printf("proximity_pos B %f %f\n", d(0),d(1));
-			return d(0)*d(1);
+		TType proximity_pos(const Transformation &o, const TDist &thr, const TType &k) const {
+			return factor(energy_distance(o-link_, thr));
 		}
 
 		TDist proximity(const boost::math::normal distribution[2]) const {
