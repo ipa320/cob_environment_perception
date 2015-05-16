ec3a662e461296b49c1d93829a48c76c681e07a8
ipa-josh <joshua.hampp@ipa.fraunhofer.de>
2015-05-16 06:44:56 +0200

    cleaning

--- cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/mapping.h
+++ cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/mapping.h
@@ -8,11 +8,8 @@
 namespace cob_3d_experience_mapping {
 	namespace algorithms {
 		
-	#include "algo/algo_1.hpp"
-	#include "algo/algo_2.hpp"
-	#include "algo/algo_3.hpp"
-	#include "algo/path_integration.hpp"
-	
-	#include "algo/step.hpp"
+		#include "algo/path_integration.hpp"
+		#include "algo/step.hpp"
+		
 	}
 }
