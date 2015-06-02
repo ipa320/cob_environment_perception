a6372c0c56da5ae4edaaa3f06f6bb28cb2fe99c5
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2015-06-02 09:00:47 +0200

    went on with shortest path

--- cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/algo/shortest_path.hpp
+++ cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/algo/shortest_path.hpp
@@ -1,36 +1,66 @@
 //find shortest path between two nodes
 
 template<class TDijkstra, class TNode, class TGraph>
-boost::shared_ptr<typename TDijkstra::Path> graph_shortest_path(const TNode &src, const TNode &tgt, TGraph &graph) {
-	TDijkstra dijkstra(graph, typename TDijkstra::LengthMap());
+bool graph_shortest_path(boost::shared_ptr<TDijkstra> &dijkstra, const TNode &src, const TNode &tgt, TGraph &graph) {
+	dijkstra.reset(new TDijkstra(graph, typename TDijkstra::LengthMap(graph,1)));
 	//dijkstra.distMap(dist);
-	dijkstra.init();
-	dijkstra.addSource(src);
-	dijkstra.start(tgt);
+	dijkstra->init();
+	dijkstra->addSource(src);
+	dijkstra->start(tgt);
 	
-	if(!dijkstra.reached(tgt)) {
+	if(!dijkstra->reached(tgt)) {
 		ROS_INFO("could not find a path");
-		return boost::shared_ptr<typename TDijkstra::Path>();
+		return false;
 	}
 	
-	ROS_INFO("found path with dist %f", dijkstra.path(tgt).length());
+	ROS_INFO("found path with dist %f", (float)dijkstra->path(tgt).length());
 		
-	return boost::shared_ptr<typename TDijkstra::Path>(new typename TDijkstra::Path(dijkstra.path(tgt)));
+	return true;
 }
 
 template<class TAction, class TCell, class TGraph, class TContext, class TMapCells, class TMapTransformations, class TTransformation>
 TAction find_next_action(const TCell &src, const TCell &tgt, TGraph &graph, TContext &ctxt, TMapCells &cells, TMapTransformations &trans)
 {
-	typedef typename lemon::Dijkstra<TGraph>::Path TPath;
+	typedef typename lemon::Dijkstra<TGraph> TSearchAlgo;
+	typedef typename TContext::TState TState;
+	typedef typename TState::TArcOutIterator TArcIter_out;
 	
-	boost::shared_ptr<TPath> path = graph_shortest_path<lemon::Dijkstra<TGraph> >(src->node(), tgt->node(), graph);
-	if(!path) return TAction::None();
-	if(path->length()<=1) return TAction::Reached();
+	boost::shared_ptr<TSearchAlgo> res;
+	if(!graph_shortest_path<TSearchAlgo>(res, src->node(), tgt->node(), graph))
+		return TAction::None();
 	
-	return TAction(trans[path->nthIt(0)]);
+	typename TGraph::Node next_node, tmp=tgt->node();
+	while(tmp!=src->node()) {
+		next_node = tmp;
+		tmp = res->predNode(tmp);
+	}
+	
+	if(next_node==tgt->node()) {
+		ROS_INFO("reached");
+		return TAction::Reached();
+	}
+	
+	for(TArcIter_out ait(src->arc_out_begin(graph)); ait!=src->arc_out_end(graph); ++ait) {
+		if(ctxt.current_active_cell()->opposite_node(graph, ait)==next_node) {
+			ROS_INFO("found next action");
+			return TAction( trans[ait] );
+		}
+	}
+	
+	ROS_ERROR("run in some error...");
+	return TAction::None();
 	
 	/*for(int i=0; i<path->length(); i++) {
 		typename TPath::ArcIt it = path->nthIt(i);
-	}*/
+	}
+	
+	
+    for (Node v=t;v != s; v=dijkstra_test.predNode(v)) {
+      std::cout << g.id(v) << "<-";
+    }
+    
+    
+	* 
+	* */
 }
 
