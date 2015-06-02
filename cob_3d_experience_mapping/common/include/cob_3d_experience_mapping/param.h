b29508494a7a65e078c983abcde46e6668516e98
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2015-06-02 08:59:43 +0200

    updated params

--- cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/param.h
+++ cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/param.h
@@ -15,16 +15,31 @@ namespace cob_3d_experience_mapping {
 		TEnergyFactor algo2_inhibition_constant_;
 		
 		TDist prox_thr_;
-		typename TDist::Scalar energy_const_;
+		typename TDist::Scalar energy_max_;
 		
 		int est_occ_;
 		int min_age_;
+		int max_active_cells_;
 
 		//visualization
 		std_msgs::ColorRGBA vis_color_cell_;
 
 		dynamic_reconfigure::Server<cob_3d_experience_mapping::SettingsConfig> server_settings_;
 		dynamic_reconfigure::Server<cob_3d_experience_mapping::Vis_SettingsConfig> server_vis_settings_;
+		
+		template<class Archive>
+		void serialize(Archive & ar, const unsigned int version)
+		{
+		    ROS_ASSERT(version==0); //TODO: version handling
+		    
+		    for(int i=0; i<prox_thr_.rows(); i++)
+				for(int j=0; j<prox_thr_.cols(); j++)
+					ar & BOOST_SERIALIZATION_NVP(prox_thr_(i,j));
+		    ar & BOOST_SERIALIZATION_NVP(est_occ_);
+		    ar & BOOST_SERIALIZATION_NVP(min_age_);
+		    ar & BOOST_SERIALIZATION_NVP(max_active_cells_);
+		    ar & BOOST_SERIALIZATION_NVP(energy_max_);
+		}
 
 		Parameter() {
 			//TODO: read from parameter server
@@ -34,13 +49,12 @@ namespace cob_3d_experience_mapping {
 		}
 
 		void cb_settings(cob_3d_experience_mapping::SettingsConfig &config, uint32_t level) {
-			//algo1_energy_weight_ = config.algo1_energy_weight;
-			//algo2_inhibition_constant_ = config.algo2_inhibition_constant;
 			prox_thr_(0) = config.translation;
 			prox_thr_(1) = config.rotation;
-			energy_const_ = config.energy_constant;
+			energy_max_ = config.energy_max;
 			est_occ_ = config.est_occ;
 			min_age_ = config.min_age;
+			max_active_cells_ = config.max_active_cells;
 
 			ROS_INFO("updated settings");
 		}
