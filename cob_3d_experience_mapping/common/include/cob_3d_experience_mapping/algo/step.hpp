8d9b2019fe7fd2376ccd4841901bdf36340db90e
ipa-josh <joshua.hampp@ipa.fraunhofer.de>
2015-06-12 12:38:33 +0200

    update

--- cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/algo/step.hpp
+++ cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/algo/step.hpp
@@ -25,11 +25,16 @@ void step(TGraph &graph, TContext &context, TMapCells &cells, TMapTransformation
 	context.clean_active_list();
 	DBG_PRINTF("no. cells: %d", (int)context.active_cells().size());
 	
+	typename TState::TPtr tgt = context.active_cells()[rand()%context.active_cells().size()];
 	ActionSearchResult<TTransformation> action = find_next_action<ActionSearchResult<TTransformation>, typename TState::TPtr, TGraph, TContext, TMapCells, TMapTransformations, TTransformation>
-	(context.virtual_cell(), context.current_active_cell(), graph, context, cells, trans);
-	ROS_INFO("test shortest path: %d %d", (int)action.found(), (int)action.reached());
+	(context.virtual_cell(), tgt, graph, context, cells, trans);
+	DBG_PRINTF("test shortest path: %d %d", (int)action.found(), (int)action.reached());
 	
-	save_content(ContextContainer<TContext,TGraph,TMapCells,TMapTransformations>(context, graph, cells, trans), "/tmp/exp_mapping.xml");
+	ActionSearchResult<TTransformation> action2 = find_next_action<ActionSearchResult<TTransformation>, typename TState::TPtr, TGraph, TContext, TMapCells, TMapTransformations, TTransformation>
+	(tgt, context.virtual_cell(), graph, context, cells, trans);
+	DBG_PRINTF("test REVERSE shortest path: %d %d", (int)action2.found(), (int)action2.reached());
+	
+	cob_3d_experience_mapping::serialization::save_content<boost::archive::binary_oarchive>(ContextContainer<TContext,TGraph,TMapCells,TMapTransformations>(context, graph, cells, trans), "/tmp/exp_mapping.xml.zip", true);
 	
 	//ContextContainer<TContext,TGraph,TMapCells,TMapTransformations> container(context, graph, cells, trans);
 	//restore_content(container, "/tmp/exp_mapping.xml");
--- cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/types/serialization.h
+++ cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/types/serialization.h
@@ -2,40 +2,102 @@
 
 #include <fstream>
 #include <boost/serialization/nvp.hpp>
+
 #include <boost/archive/xml_iarchive.hpp>
 #include <boost/archive/xml_oarchive.hpp>
 
-/*namespace boost {
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+/*
+ * possible arhives:
+ *  - boost::archive::xml_oarchive
+ *  - boost::archive::text_oarchive
+ *  - boost::archive::binary_oarchive
+ */
+ 
+#include <boost/iostreams/filtering_stream.hpp>
+#include <boost/iostreams/filter/zlib.hpp>
+
+namespace cob_3d_experience_mapping{
 namespace serialization {
 
-template<class Archive, class Type>
-void serialize(Archive & ar, Type & obj, const unsigned int version)
-{
-    obj.serialize(ar,version);
-}
-
-} // namespace serialization
-} // namespace boost*/
-
-
-template<class TContent>
-void save_content(const TContent &s, const char * filename){
-    // make an archive
-    std::ofstream ofs(filename);
-    assert(ofs.good());
-    boost::archive::xml_oarchive oa(ofs);
+template<class TArchive, class TContent>
+void export_content(const TContent &s, std::ostream &ostr){
+    TArchive oa(ostr);
     oa << BOOST_SERIALIZATION_NVP(s);
 }
 
+template<class TArchive, class TContent>
+void import_content(TContent &s, std::ostream &istr)
+{
+    TArchive ia(istr);
+
+    // restore the schedule from the archive
+    ia >> BOOST_SERIALIZATION_NVP(s);
+}
+
+template<class TArchive, class TContent>
+void export_content_compr(const TContent &s, std::ostream &_ostr){
+	boost::iostreams::filtering_ostream ostr;
+    ostr.push(boost::iostreams::zlib_compressor());
+    ostr.push(_ostr);
+    
+    TArchive oa(ostr);
+    oa << BOOST_SERIALIZATION_NVP(s);
+}
+
+template<class TArchive, class TContent>
+void import_content_compr(TContent &s, std::ostream &_istr)
+{
+	boost::iostreams::filtering_istream istr;
+    istr.push(boost::iostreams::zlib_decompressor());
+    istr.push(_istr);
+    
+    TArchive ia(istr);
+
+    // restore the schedule from the archive
+    ia >> BOOST_SERIALIZATION_NVP(s);
+}
+
+
+
+template<class TArchive, class TContent>
+void save_content(const TContent &s, const char * filename, const bool compression=false){
+    // make an archive
+    std::ofstream ofs(filename);
+    assert(ofs.good());
+    if(compression)
+		export_content_compr<TArchive>(s, ofs);	
+    else
+		export_content<TArchive>(s, ofs);
+}
+//default archive
 template<class TContent>
+void save_content(const TContent &s, const char * filename, const bool compression=false){
+	save_content<boost::archive::xml_oarchive>(s,filename,compression);
+}
+
+template<class TArchive, class TContent>
 void
-restore_content(TContent &s, const char * filename)
+restore_content(TContent &s, const char * filename, const bool compression=false)
 {
     // open the archive
     std::ifstream ifs(filename);
     assert(ifs.good());
-    boost::archive::xml_iarchive ia(ifs);
+    if(compression)
+		import_content_compr<TArchive>(s, ifs);
+	else
+		import_content<TArchive>(s, ifs);
+}
+//default archive
+template<class TContent>
+void restore_content(TContent &s, const char * filename, const bool compression=false) {
+	restore_content<boost::archive::xml_iarchive>(s,filename,compression);
+}
 
-    // restore the schedule from the archive
-    ia >> BOOST_SERIALIZATION_NVP(s);
+
+}	//namespaces
 }
