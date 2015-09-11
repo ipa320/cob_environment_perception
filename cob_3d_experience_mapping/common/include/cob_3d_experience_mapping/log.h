f3023afbd86547097603d2420303176e49773a2b
Joshua Hampp <joshua.hampp@ipa.fraunhofer.de>
2015-09-11 11:12:21 +0200

    log update

--- cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/log.h
+++ cob_3d_experience_mapping/common/include/cob_3d_experience_mapping/log.h
@@ -1,13 +1,19 @@
 #pragma once
 
+//#define NDEBUG
+
 #ifdef NDEBUG
 
 #define DBG_PRINTF(str, ...) ;
 #define ERROR_PRINTF(str, ...) ROS_ERROR(str, ##__VA_ARGS__)
 
+#define DBG_PRINTF_URGENT(str, ...) ;
+//#define DBG_PRINTF_URGENT(str, ...) {printf(str, ##__VA_ARGS__);fflush(stdout);}
+
 #else
 
 #define DBG_PRINTF(str, ...) {printf(str, ##__VA_ARGS__);fflush(stdout);}
 #define ERROR_PRINTF(str, ...) ROS_ERROR(str, ##__VA_ARGS__)
+#define DBG_PRINTF_URGENT(str, ...) {printf(str, ##__VA_ARGS__);fflush(stdout);}
 
 #endif
