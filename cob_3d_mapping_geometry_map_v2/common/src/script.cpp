fa007c379698939f50c607ded0fbdd2d09f86c28
ipa-josh <joshua.hampp@ipa.fraunhofer.de>
2015-12-07 09:10:06 +0100

    removed angelscript

--- cob_3d_mapping_geometry_map_v2/common/src/script.cpp
+++ cob_3d_mapping_geometry_map_v2/common/src/script.cpp
@@ -1,15 +1,3 @@
-
-#include <angelscript.h>
-
 #include <cob_3d_mapping_geometry_map_v2/types/object.h>
 
 using namespace cob_3d_geometry_map;
-
-bool register_obj(asIScriptEngine *engine, Object *obj, const std::string &inst_name) {
-	assert(obj);
-	
-	int r = engine->RegisterGlobalProperty( ("Map::"+obj->type()+" "+inst_name).c_str(), obj);
-	assert(r>=0);
-	
-	return r>=0;
-}
